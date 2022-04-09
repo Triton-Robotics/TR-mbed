8a72f9484f6fe68f0d70bf87ca5381828916afe4
Mew463 <mwyeoh123@gmail.com>
2022-04-09 15:47:05 -0700

    fixed from conflicts

--- util/motor/motor.hpp
+++ util/motor/motor.hpp
@@ -10,11 +10,8 @@
 //////////////////////////////////////////////
 
 static CAN can1(PA_11,PA_12,1000000);
-<<<<<<< Updated upstream
 //static CAN can1(PB_12,PB_13,1000000);
 static CAN can2(PB_12,PB_13,1000000);
-=======
->>>>>>> Stashed changes
 
 enum motorMode {DISABLED, POSITION, SPEED, CURRENT};
 
--- util/motor/motorcan2.hpp
+++ util/motor/motorcan2.hpp
@@ -1,7 +1,6 @@
 #include "mbed.h"
 #include "../algorithms/pid.hpp"
 #include <cstdint>
-<<<<<<< Updated upstream
 #ifndef motor2_hpp
 #define motor2_hpp
 #include "motor.hpp"
@@ -11,20 +10,10 @@
 
 class MotorCan2 : public Motor {
     public:
-        MotorCan2(int canNum, motorType type = STANDARD, int ratio = 19, int inverted = false) : Motor(canNum,type,ratio,inverted) {MotorCan2::canOutput = 2;}
+        MotorCan2(int canNum, motorType type = STANDARD, int ratio = 19, int inverted = false) : Motor(canNum,type,ratio,inverted) {//MotorCan2::canOutput = 2;
+        }
         //using Motor::Motor;
         //canOutput = 2;
-=======
-#ifndef motor_hpp
-#define motor_hpp
-#include "motor.hpp"
-#include "../communications/CANMsg.h"
 
-static CAN can1(PB_12,PB_13,1000000);
-
-class MotorCan2 : Motor {
-
-
->>>>>>> Stashed changes
 };
 #endif
