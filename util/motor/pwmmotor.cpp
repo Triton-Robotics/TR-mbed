b537985c0b47f16b385db917df0b8b867f2e8f71
Mew463 <mwyeoh123@gmail.com>
2022-04-12 22:28:31 -0700

    updated pwmmotor class/documentation

--- util/motor/pwmmotor.cpp
+++ util/motor/pwmmotor.cpp
@@ -1,24 +1,29 @@
 #include "mbed.h"
 
-class PWMMotor : PwmOut{
-    public: 
-
-    /**
+/**
     * @brief Construct a new PWM Output 
     * @param pin 
-    * @param defaultVal from 0 - 0.99f
+    * @param defaultVal integerfrom 0 - 180 corresponds with a pulse width from 1ms to 2ms which is default for most servos/pwm motor controllers
     */
-    PWMMotor(PinName pin, double defaultVal = 0) : PwmOut(pin) {
-        period(.00001f);
-        write(defaultVal);
-    }                                                                               
+class PWMMotor : PwmOut{
 
-    /**
-    * @brief Set PWM Output 
-    * @param motorVal double from 0 - 0.99
-    */
-    void set(double motorVal) {
-        write(motorVal);
-    }
+    private:
+        float intToPulse(int val) {
+            return .001 + val * .001/180;
+        }         
+
+    public: 
+        PWMMotor(PinName pin, int defaultVal = 0) : PwmOut(pin) {
+            period(.02);
+            pulsewidth(defaultVal);
+        }            
+
+        /**
+        * @brief Set PWM Output 
+        * @param motorVal integer from 0 - 180 which corresponds to a pulse width from 1ms to 2ms
+        */
+        void set(int motorVal) {
+            pulsewidth(intToPulse(motorVal));
+        }
 
 };
