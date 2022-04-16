73e920f1987906a412d55e9f9c0a18badaf598c5
pknessness <93059296+pknessness@users.noreply.github.com>
2022-04-15 18:53:58 -0700

    turret subsystem no errors

--- src/main.hpp
+++ src/main.hpp
@@ -1,5 +1,7 @@
 #include "mbed.h"
 #include "../util/motor/motor.hpp"
-#include "../util/communications/include/DJIRemote.hpp"
+//#include "../util/communications/include/DJIRemote.hpp"
 #include "../util/communications/canHandler.hpp"
 #include "../util/algorithms/pid.hpp"
+
+enum robotType{TEST_BENCH, INFANTRY, HERO, ENGINEER, SENTRY};
---
+++ src/subsystems/TurretSubsystem.hpp
@@ -0,0 +1,12 @@
+#include "mbed.h"
+#include "motor.hpp"
+#ifndef turret_subsystem_cpp
+#define turret_subsystem_cpp
+
+//enum turretMode{RELATIVE, ANGULAR};
+
+class TurretSubsystem(){
+
+};
+
+#endif
