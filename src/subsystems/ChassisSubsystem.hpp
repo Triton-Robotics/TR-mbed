48b67209ff799197e4687da1c2d2a89572afaff1
pknessness <93059296+pknessness@users.noreply.github.com>
2022-04-15 19:26:07 -0700

    chassissubsystem template

--- src/subsystems/ChassisSubsystem.hpp
+++ src/subsystems/ChassisSubsystem.hpp
@@ -7,7 +7,16 @@
 enum chassisMode{TANK, MECANUM, BEYBLADE, RIGHT_ANGLE_MECANUM};
 
 class ChassisSubsystem{
+    public:
+        int x;
+        int y;
+        //Motor motor1;
 
+        ChassisSubsystem(int parameter1, double para2){
+            //do something here
+        }
+
+        //add methods to make it easy for an end-user to use chassis without worry.
 };
 
 #endif
