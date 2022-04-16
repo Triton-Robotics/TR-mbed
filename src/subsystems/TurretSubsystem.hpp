a410c8984056375ec5b619e589f086494e008bd1
pknessness <93059296+pknessness@users.noreply.github.com>
2022-04-15 19:14:32 -0700

    turret subsystem
    part of the bare-bones of the class is in. its definitily not gonna work in its current state, plus i have to set up wrap around so that it goes the direction closest to where it needs to be.
    
    also, random error that i dont know what its about, it appeared as soon as i created the class but there was literally nothing except for a blank constructor.... idk:
    
    Expected unqualified-idclang(expected_unqualified_id)
    
    thats the error id.

--- src/subsystems/TurretSubsystem.hpp
+++ src/subsystems/TurretSubsystem.hpp
@@ -1,11 +1,39 @@
 #include "mbed.h"
-#include "motor.hpp"
+#include "../../util/motor/motor.hpp"
+#include "../../util/motor/pwmmotor.cpp"
+#include <cstdlib>
+#include <algorithm>
 #ifndef turret_subsystem_cpp
 #define turret_subsystem_cpp
 
-//enum turretMode{RELATIVE, ANGULAR};
+enum turretMode{RELATIVE, ANGULAR};
 
 class TurretSubsystem(){
+    public:
+        Motor* gimbalX;
+        Motor* gimbalY;
+        PWMMotor flywheelL;
+        PWMMotor flywheelR;
+        Motor* serializer;
+        int xBounds = {0,0} //angle array for the x axis as a bound for the amount of allowed rotation;
+        int YBounds = {0,0} //angle array for the y axis as a bound for the amount of allowed rotation;
+
+        TurretSubsystem(Motor gX, Motor gY, PWMMotor flyL, PWMMotor flyR, Motor serialize)
+        {
+            gimbalX = &gX;
+            gimbalY = &gY;
+            flywheelL = &flyL;
+            flywheelR = &flyR;
+            serializer = &serialize;
+        }
+
+        gimbalMove(int angleX, int angleY){
+            angleX = std::min(std::max(xBounds[1],angleX),xBounds[0]);
+            angleY = std::min(std::max(yBounds[1],angleY),yBounds[0]);
+            gimbalX.setDesiredPosition(angleX);
+            gimbalY.setDesiredPosition(angleY);
+        }
+
 
 };
 
