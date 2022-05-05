11e8f9e7017543bda0ebfc838a38bbd2d8ec3789
pknessness <93059296+pknessness@users.noreply.github.com>
2022-05-04 20:10:08 -0700

    expereiment where i delete boba off my device and my device only

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,10 @@ int main(){
             
         }
     }else if(rType == SENTRY){
-        ChassisSubsystem chassis(3,4,0,0,CANHandler::CANBUS_1,M3508);
+        //ChassisSubsystem chassis(3,4,0,0,CANHandler::CANBUS_1,M3508);
+        Motor chassis1(3,CANHandler::CANBUS_1,M3508);
+        Motor chassis2(4,CANHandler::CANBUS_1,M3508);
+
         Motor gimbalX(2,CANHandler::CANBUS_1,GM6020);
         Motor gimbalY(5,CANHandler::CANBUS_1,GM6020);
         Motor indexer(7,CANHandler::CANBUS_1,M3508);
@@ -26,15 +29,17 @@ int main(){
             
         }
     }else if(rType == INFANTRY){
-        ChassisSubsystem chassis(1,2,3,4,CANHandler::CANBUS_1,M3508);
-        //Motor gimbalX(5,CANHandler::CANBUS_1,GM6020); //NONE OF THESE IDs ARE CORRECT
-        //Motor gimbalY(6,CANHandler::CANBUS_1,GM6020); //NONE OF THESE IDs ARE CORRECT
+        ChassisSubsystem chassis(4,2,1,3,CANHandler::CANBUS_1,M3508);
+        Motor gimbalX(5,CANHandler::CANBUS_1,GM6020); //NONE OF THESE IDs ARE CORRECT
+        Motor gimbalY(6,CANHandler::CANBUS_1,GM6020); //NONE OF THESE IDs ARE CORRECT
         while(1){
             myremote.remoteUpdate(); //remoteController.read();
             chassis.move(
                 myremote.getStickData(LEFTJOYY, 0, maxspeed), //x
                 myremote.getStickData(LEFTJOYX, 0, maxspeed), //y
                 myremote.getStickData(RIGHTJOYX, 0, maxspeed)); //rx
+            gimbalY.setDesiredSpeed(myremote.getStickData(RIGHTJOYY, 0, maxspeed));
+            gimbalX.setDesiredSpeed(myremote.getStickData(WHEEL, 0, maxspeed));
         }
     }else if(rType == HERO){
         ChassisSubsystem chassis(1,2,3,4,CANHandler::CANBUS_1,M3508);
