1f477f5db137b2376dfbe15bae68bd227293b85f
pknessness <93059296+pknessness@users.noreply.github.com>
2022-06-06 01:33:03 -0700

    untested engineer code

--- Engineer/main.cpp
+++ Engineer/main.cpp
@@ -1,81 +1,95 @@
+#include "mbed.h"
 #include "../src/main.hpp"
 
-//DJIRemote myremote(PA_0, PA_1);
-//CANHandler canPorts(PA_11,PA_12,PB_12,PB_13);
+DigitalInOut clawPin(PA_13);
+DigitalInOut rfidPin(PA_14);
 
-ChassisSubsystem awesomechassis(4,2,1,3, CANHandler::CANBUS_1, C620);
+NewChassisSubsystem chassis(4,2,1,3, CANHandler::CANBUS_1, C620);
 
-//Motor yaw(5, CANHandler::CANBUS_1, GIMBLY);
-Motor pitch(6, CANHandler::CANBUS_1, GIMBLY);
-Motor serializer(7, CANHandler::CANBUS_1, C610);
+CANMotor gimbalX(3,CANHandler::CANBUS_1,M3508);
+CANMotor gimbalY(6,CANHandler::CANBUS_1,GM6020);
 
-int neutralval = 0;
-int revval = 75;
+PWMMotor leftFlywheel(PA_5);
+PWMMotor rightFlywheel(PA_6);
 
+CANMotor indexer(7,CANHandler::CANBUS_1,M2006);
 
-PWMMotor RFLYWHEEL(D12, neutralval);
-PWMMotor LFLYWHEEL(D11, neutralval);
+int maxspeed = 500;
 
-int yawval = 0;
-int pitchval = 220;
-int maxspeed = 300;
-int maxpitchyawchange = 5;
-
-bool checkJam(Motor *mymotor) {
-    static int mycount = 0;
-
-    if (mymotor->getData(VELOCITY) == 0) 
-        mycount++;
-    else
-        mycount = 0;
-    
-    if (mycount > 10)
-        return true;
-    else
-        return false;
-}
+// main() runs in its own thread in the OS
 
 int main()
 {
-    Motor::setCANHandler(&canPorts);
-    pitch.zeroPos();
+    threadingRemote.start(&remoteThread);
+    CANMotor::setCANHandlers(&canHandler1,&canHandler2);
+    clawPin.output();
+    rfidPin.output();
+    //Motor::setCANHandler(&canPorts);
+    //chassis1.pidSpeed.setPID(.1, 0, 0);
+    //chassis1.pidPosition.setPID(.08,0,0.0125);
+    indexer.outCap = 7000;
 
-    while (true) {
-        myremote.remoteUpdate();
+        while(1){
 
-        if (myremote.getSwitchData(RSWITCH) == 2) {
-            awesomechassis.move(myremote.getStickData(LEFTJOYY, 0, maxspeed), myremote.getStickData(LEFTJOYX, 0, maxspeed), myremote.getStickData(RIGHTJOYX, 0, maxspeed));
-        }
-        else {
-            //yawval+= (int)myremote.getStickData(LEFTJOYY, 0, maxpitchyawchange);
-            pitchval+=(int)myremote.getStickData(LEFTJOYY, 0, maxpitchyawchange);
+            if(rS == 2){
+                //chassis1.setSpeed(lX);
+                //chassis2.setSpeed(lX);
+            }else{
+                //chassis1.setPower(lX);
+                //chassis2.setPower(lX);
+            }
+            chassis.movePow(lY,lX,Wh);
 
-            if (pitchval > 240)
-                pitchval = 240;
-            if (pitchval < 180)
-                pitchval = 180;
+            //gimbalX.setPosition(rX);
+            //gimbalY.setPosition(rY);
+
+            gimbalX.setPower(rX * 2);
+            gimbalY.setPower(rY * 6);
+
+            if(lS == 2){
+                clawPin = 0;
+                rfidPin = 0;
+            }else if(lS == 3){
+                clawPin = 1;
+                rfidPin = 0;
+            }else if(lS == 1){
+                clawPin = 0;
+                rfidPin = 1;
+            }
             
-        }
-        
-        if (myremote.getSwitchData(LSWITCH) == 1) {
-            LFLYWHEEL.set(revval);
-            RFLYWHEEL.set(revval);
-            serializer.setDesiredSpeed(-70);
-        }
-        else if (myremote.getSwitchData(LSWITCH) == 2) {
-            serializer.setDesiredSpeed(90);
-        }
-        else if (myremote.getSwitchData(LSWITCH) == 3) {
-            LFLYWHEEL.set(neutralval);
-            RFLYWHEEL.set(neutralval);
-            serializer.setDesiredSpeed(0);
+            int indexJamTime = 0;
+            if(rS == 2){
+                leftFlywheel.set(0);
+                rightFlywheel.set(0); 
+                indexer.setPower(0);
+            }else if(rS == 3){
+                leftFlywheel.set(60);
+                rightFlywheel.set(60); 
+                if(abs(indexer.getData(TORQUE)) > 1000 & abs(indexer.getData(VELOCITY)) < 20){ //jam
+                    indexJamTime = us_ticker_read() /1000;
+                }
+                if(us_ticker_read() / 1000 - indexJamTime < 500){
+                    indexer.setPower(-7000); //jam
+                    printf("JAMMMMM- ");
+                }else if(us_ticker_read() / 1000 - indexJamTime < 750){
+                    indexer.setPower(7000); //jam
+                    printf("POWER FORWARD- ");
+                }else{
+                    indexer.setPower(1700);
+                }
+                printf("AUTO-PWR:%d Jam-Free:%dms TORQ:%d, VELO:%d\n",indexer.powerOut,us_ticker_read() / 1000 - indexJamTime, indexer.getData(TORQUE), indexer.getData(VELOCITY));
+            }else if(rS == 1){
+                leftFlywheel.set(60);
+                rightFlywheel.set(60); 
+                indexer.setPower(rY*4);
+                printf("MANUAL-PWR:%d VELO:%d\n", indexer.powerOut, indexer.getData(VELOCITY));
+            }
+
+            //chassis2.setSpeed(lY);
+            //CANMotor::tick();
+            //remotePrint();
         }
 
-        //yaw.setDesiredPos(yawval);
-        pitch.setDesiredPos(220);
-
-        
-
-    }
 }
+
 
