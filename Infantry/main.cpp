95f4d692e90a5ca342364715a0c6fba1f918b814
Mew463 <mwyeoh123@gmail.com>
2022-06-28 17:14:00 -0500

    post 2022 comp

--- Infantry/main.cpp
+++ Infantry/main.cpp
@@ -19,6 +19,11 @@ CANMotor indexer(7, NewCANHandler::CANBUS_1, C610);
 int indexJamTime = 0;
 int lastJam = 0;
 
+unsigned long cT = 0;
+unsigned long forwardTime = 250;
+unsigned long reverseTime = 300;
+unsigned long totalTime;
+
 PWMMotor RFLYWHEEL(D12); PWMMotor LFLYWHEEL(D11);
 PWMMotor flyWheelMotors[] = {RFLYWHEEL, LFLYWHEEL};
 
@@ -29,9 +34,10 @@ void setFlyWheelPwr(int pwr) {
 
 int main()
 {
+    
     threadingRemote.start(&remoteThread);
     //threadingReferee.start(&refereeThread);
-    CANMotor::setCANHandlers(&canHandler1,&canHandler2);
+    CANMotor::setCANHandlers(&canHandler1,&canHandler2, true, false);
 
     LB.setSpeedPID(1.75, 0.351, 5.63);
     RF.setSpeedPID(1.073, 0.556, 0);
@@ -70,7 +76,7 @@ int main()
             LB.setPower(LBa * powmultiplier);
             RB.setPower(-RBa * powmultiplier);
             
-            pitch.setPower(rY*5);
+            pitch.setPower(rY*9);
             //yaw.setSpeed(rX/100);
             
 
@@ -98,28 +104,38 @@ int main()
         }
 
         if (lS == 3) {
+            indexer.setPower(-5000);
             
         }else if(lS == 2){ //disable serializer
             indexer.setPower(0);
             setFlyWheelPwr(0);
         }else if(lS == 1){
-            setFlyWheelPwr(30);
-                if(abs(indexer.getData(TORQUE)) > 500 & abs(indexer.getData(VELOCITY)) < 20){ //intial jam detection
-                    if (lastJam == 0) {
-                        indexJamTime = us_ticker_read() /1000; // start clock
-                        //indexer.setPower(-7000);
-                        lastJam = 1;
-                        printf("jam detected!\n");
-                    }
-                }
-                else 
-                    lastJam = 0;
+            setFlyWheelPwr(40);
+            cT = us_ticker_read()/1000;
+            totalTime = forwardTime + reverseTime;
+
+            if (cT % totalTime > 0 && cT % totalTime < forwardTime) {
+                indexer.setPower(-2000); // shoot
+            }
+            else {
+                indexer.setPower(800); //unjam
+            }
+                // if(abs(indexer.getData(TORQUE)) > 500 & abs(indexer.getData(VELOCITY)) < 50){ //intial jam detection
+                //     if (lastJam == 0) {
+                //         indexJamTime = us_ticker_read() /1000; // start clock
+                //         //indexer.setPower(-7000);
+                //         lastJam = 1;
+                //         printf("jam detected!\n");
+                //     }
+                // }
+                // else 
+                //     lastJam = 0;
                 
-                if(lastJam && us_ticker_read() / 1000 - indexJamTime > 100){ // If jam for more than 75ms then reverse
-                    indexer.setPower(7500); 
-                    printf("Unjamming...\n");
-                }else
-                    indexer.setSpeed(-75*36); // No Jam, regular state
+                // if(lastJam && us_ticker_read() / 1000 - indexJamTime > 75){ // If jam for more than 75ms then reverse
+                //     indexer.setPower(10000); 
+                //     printf("Unjamming...\n");
+                // }else
+                //     indexer.setSpeed(-75*36); // No Jam, regular state
         }
 
         ThisThread::sleep_for(1ms);
