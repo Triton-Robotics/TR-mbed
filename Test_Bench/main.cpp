ffe7748dbf77f17fa4b12f77aa570534a0f57c0f
GioByte10 <gvanni.bernal10@gmail.com>
2023-01-19 20:16:47 -0800

    Modified IMU example; Added Quaternions

--- Test_Bench/main.cpp
+++ Test_Bench/main.cpp
@@ -1,27 +1,18 @@
-#include "../src/main.hpp"
-#include "../util/imu/BNO055.cpp"
 #include <cstdlib>
-//#include "../util/communications/DJIRemote.cpp"
-
-// #include "o-led/Adafruit_SSD1306.h"
-// #include "o-led/Adafruit_SSD1306.cpp"
-// #include "o-led/Adafruit_GFX.h"
-// #include "o-led/Adafruit_GFX.cpp"
-// #include "o-led/Adafruit_GFX_Config.h"
-
-// SPIPreInit gSpi(p5,NC,p7);
-// Adafruit_SSD1306_Spi gOled1(gSpi,p26,p25,p24);
-
-//I2C gI2C(PB_9,PB_8);
-//Adafruit_SSD1306_I2c gOled2(gI2C,PA_13);
+#include <iostream>
+#include "mbed.h"
+#include "../src/main.hpp"
+#include "../util/imu/BNO055.h"
+#include "../util/imu/BNO055.cpp"
 
 I2C    i2c(PB_9, PB_8);                // SDA, SCL
 BNO055 imu(i2c, PA_8);
 
 BNO055_ID_INF_TypeDef bno055_id_inf;
 BNO055_EULER_TypeDef  euler_angles;
+BNO055_QUATERNION_TypeDef   quat;
 
-// sentry only from 1230 - 1830
+// sentry only from 1230 1830
 
 CANMotor m3508_1(1,NewCANHandler::CANBUS_1,M3508);
 CANMotor m3508_2(2,NewCANHandler::CANBUS_1,M3508);
@@ -38,71 +29,75 @@ CANMotor gimbly11(7,NewCANHandler::CANBUS_1,GIMBLY);
 DigitalOut led(LED1);
 DigitalIn button(BUTTON1);
 
-//Remote remote1(PA_3);
+
 
 CANMotor getMotor(int i){
     switch (i) {
         
         case 1:
             return m3508_1;
-            break;
+            
         case 2:
             return m3508_2;
-            break;
+            
         case 3:
             return m3508_3;
-            break;
+            
         case 4:
             return m3508_4;
-            break;
+            
         case 5:
             return m3508_5;
-            break;
+            
         case 6:
             return m3508_1;
-            break;
+            
         case 7:
             return m3508_1;
-            break;
+            
         case 8:
             return gimbly8;
-            break;
+            
         case 9:
             return gimbly9;
-            break;
+            
         case 10:
             return gimbly10;
-            break;
+            
         case 11:
             return gimbly11;
-            break;
+            
         default:
             return m3508_1;
-            break;
+            
     }
 }
 
-int main()
-{
+void getAnglesFromQuat(int &yaw, int &pitch, int &roll){
+
+    imu.get_quaternion(&quat);
+    double yy = quat.y * quat.y;        // 2 Uses below
+
+    roll = atan2(2 * (quat.w * quat.x + quat.y * quat.z), 1 - 2*(quat.x * quat.x + yy));
+    pitch = asin(2 * quat.w * quat.y - quat.x * quat.z);
+    yaw = atan2(2 * (quat.w * quat.z + quat.x * quat.y), 1 - 2*(yy+quat.z * quat.z));
+
+    /*  Convert Radians to Degrees */
+    roll    *= 57.2958;
+    pitch   *= 57.2958;
+    yaw     *= 57.2958;
+
+}
+
+int main(){
+
+    imu.change_fusion_mode(MODE_NDOF);
+
     CANMotor::setCANHandlers(&canHandler1,&canHandler2, false, false);
 
-    int numMotor = 0;
-    int stateLS = 0;
-
+    int yaw, pitch, roll;
+    int countLoops = 0, refLoop = 0;
     unsigned long loopTimer = us_ticker_read() / 1000;
-
-    // m3508_1.setSpeedPID(1.79, 0.0, 10.57);
-    // m3508_2.setSpeedPID(1.79, 0.0, 10.57);
-    // m3508_3.setSpeedPID(1.79, 0.0, 10.57);
-    // m3508_4.setSpeedPID(1.79, 0.0, 10.57);
-    // m3508_5.setSpeedPID(1.79, 0.0, 10.57);
-    // gimbly8.setPositionPID(0.5,0,0);
-    // gimbly9.setPositionPID(0.5,0,0);
-    // gimbly10.setPositionPID(0.5,0,0);
-    // gimbly11.setPositionPID(0.5,0,0);
-
-    int countLoops = 0;
-    int refLoop=0;
 
     printf("Bosch Sensortec BNO055 test program on " __DATE__ "/" __TIME__ "\n");
 
@@ -116,82 +111,30 @@ int main()
 
     while (true) {
         unsigned long timeStart = us_ticker_read() / 1000;
+        imu.get_euler_angles(&euler_angles);
+        //imu.get_quaternion(&quat);
+        //getAnglesFromQuat(yaw, pitch, roll);
+
         if(timeStart - loopTimer > 25){
             loopTimer = timeStart;
 
-            //printf("*Potato*  %d\n", countLoops);
             led = !led;
-            // if(countLoops % 50 == 1)
-            //     led = !led;
 
             remoteRead();
-            //remote1.read();
             refLoop++;
-            if(refLoop > 25){
-                // refereeThread();
+
+            if(refLoop > 25)
                 refLoop = 0;
-                //imu.get_euler_angles(&euler_angles);
-                //ThisThread::sleep_for(400ms);
-            }
+
+
 
             printf("Heading:%d [deg], Roll:%d [deg], Pitch:%d [deg]\n", (int)euler_angles.h, (int)euler_angles.r, (int)euler_angles.p);
-
-            // if(lS == 4){
-            //     // char str[80];
-            //     // sprintf(str, "%d:%d:%d:%d\n", m3508_2.getData(ANGLE),m3508_2.getData(VELOCITY),m3508_2.getData(TORQUE),m3508_2.getData(TEMPERATURE));
-            //     // printf(str);
-            // }else if(lS == 1){
-            //     // m3508_1.setSpeed(800 * (rS - 2));
-            //     // m3508_2.setSpeed(1000 * (rS - 2));
-            //     // m3508_3.setSpeed(800 * (rS - 2));
-            //     // m3508_4.setSpeed(800 * (rS - 2));
-            //     // m2006_7.setSpeed(800 * (rS - 2));
-            //     gimbly8.setPosition(2000 * (rS - 2));
-            //     gimbly9.setPosition(2000 * (rS - 2));
-            //     gimbly10.setPosition(2000 * (rS - 2));
-            //     //gimbly11.setPosition(2000 * (rS - 2));
-                
-            // }else if(lS == 3){
-            //     // printf("print:%s<\n",Robot_Commute);
-            //     m3508_1.setPower(0);
-            //     m3508_2.setPower(0);
-            //     m3508_3.setPower(0);
-            //     m3508_4.setPower(0);
-            //     m2006_7.setPower(0);
-            //     gimbly8.setPower(0);
-            //     gimbly9.setPower(0);
-            //     gimbly10.setPower(0);
-            //     //gimbly11.setPower(0);
-            // }else{
-            //     m3508_1.setPower(0);
-            //     m3508_2.setPower(0);
-            //     m3508_3.setPower(0);
-            //     m3508_4.setPower(0);
-            //     m2006_7.setPower(0);
-            //     gimbly8.setPower(0);
-            //     gimbly9.setPower(0);
-            //     gimbly10.setPower(0);
-            //     //gimbly11.setPower(0);
-            // }
-            //gimbly9.setPower(1000);
-            // if(remote1.getSwitch(Remote::Switch::LEFT_SWITCH)  == Remote::SwitchState::MID){
-            //     printf("YEETUS\n");
-            // }
-            // if(rS == 2){
-            //     oled.writeChar('a');
-            // }
-            // else
-            //     oled.writeString(0, 0, "Hello World ! 1234567890 abcdefghijklmnopqrstuvwxyz");
-
-            //gOled2.printf("YEETUS DELETuS\n");
-            //gOled2.display();
-
+            //printf("Heading:%d [deg], Roll:%d [deg], Pitch:%d [deg]\n", yaw, roll, pitch);
+            //printf("Quaternion Y:%d [deg], X:%d [deg], Z:%d [deg], W:%d [deg]\n", (int)quat.y, (int)quat.x, (int)quat.z, (int)quat.w);
             CANMotor::sendValues();
         }
 
-        imu.get_euler_angles(&euler_angles);
 
-        unsigned long timeEnd = us_ticker_read() / 1000;
         CANMotor::getFeedback();
         ThisThread::sleep_for(1ms);
         countLoops ++;
