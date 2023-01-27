66921e42cc5e6722a50d24709af737f5344c8d4c
GioByte10 <gvanni.bernal10@gmail.com>
2023-01-27 14:56:56 -0800

    Testing

--- Test_Bench/IMUmain.cpp
+++ Test_Bench/IMUmain.cpp
@@ -4,14 +4,13 @@
 #include "../util/imu/BNO055.cpp"
 
 I2C    i2c(PB_9, PB_8);                // SDA, SCL
-BNO055 imu(i2c, PA_8);
+BNO055 imu(i2c, PA_8, MODE_NDOF);
 
 BNO055_ID_INF_TypeDef bno055_id_inf;
 BNO055_ANGULAR_POSITION_typedef p;
 
 int main(){
 
-    imu.change_fusion_mode(MODE_IMU);
     imu.set_mounting_position(MT_P1);
 
     int i = 0;
@@ -23,7 +22,7 @@ int main(){
         printf("Bosch BNO055 is NOT available!!\r\n");
 
     imu.read_id_inf(&bno055_id_inf);
-    printf("CHIP:0x%02x, ACC:0x%02x, MAG:0x%02x, GYR:0x%02x, , SW:0x%04x, , BL:0x%02x\n",
+    printf("CHIP:0x%02x, ACC:0x%02x, MAG:0x%02x, GYR:0x%02x, SW:0x%04x, BL:0x%02x\n",
            bno055_id_inf.chip_id, bno055_id_inf.acc_id, bno055_id_inf.mag_id,
            bno055_id_inf.gyr_id, bno055_id_inf.sw_rev_id, bno055_id_inf.bootldr_rev_id);
 
@@ -37,7 +36,6 @@ int main(){
         sTimer += timer;
 
         //printf("Yaw:%d [deg], Roll:%d [deg], Pitch:%d [deg]\n", (int)p.yaw, (int)p.roll, (int)p.pitch);
-
 
     }
     printf("Average time:%ld [us] after %d iterations\n", sTimer/i, i);
