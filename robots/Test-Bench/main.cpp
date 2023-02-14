c1e026f3389fccc296ba1c5c78a3a1fc24f831b8
Emma <ezc7288@gmail.com>
2023-02-13 20:20:53 -0800

    testbench mutex changes

--- robots/Test-Bench/main.cpp
+++ robots/Test-Bench/main.cpp
@@ -1,6 +1,47 @@
-//
-// Created by ankit on 1/30/23.
-//
-int main(){
+#include "main.h"
+#include <cstdlib>
 
+void printCode();
+
+Mutex MUTEX; 
+Thread Thread_print_code;
+
+DJIMotor m3508_1(1,CANHandler::CANBUS_1,M3508);
+DigitalOut led(LED1);  
+
+void printCode(){
+    while(1){
+        MUTEX.lock();
+        printf("hi \n");
+        ThisThread::sleep_for(1s);
+        MUTEX.unlock();
+    }
+}
+
+int main() 
+{   
+    DJIMotor::setCANHandlers(&canHandler1,&canHandler2, false, false);
+
+    Thread_print_code.start(printCode); 
+    unsigned long loopTimer = us_ticker_read() / 1000;
+    int countLoops = 0;
+    int refLoop=0;
+
+    while (true) {
+        MUTEX.lock();
+        unsigned long timeStart = us_ticker_read() / 1000;
+        if(timeStart - loopTimer > 25){
+            loopTimer = timeStart;
+
+            led = !led;
+            remoteRead();
+            m3508_1.setPower(1000);
+            DJIMotor::sendValues();
+        }
+        unsigned long timeEnd = us_ticker_read() / 1000;
+        DJIMotor::getFeedback();
+        ThisThread::sleep_for(1s);
+        countLoops ++;
+        MUTEX.unlock();
+    }
 }
