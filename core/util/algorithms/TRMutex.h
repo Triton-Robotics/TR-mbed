fe3f96044d9442afba9b73c2443dc15a0010d602
Emma <ezc7288@gmail.com>
2023-05-10 21:30:41 -0700

    logging updates

--- core/util/algorithms/TRMutex.h
+++ core/util/algorithms/TRMutex.h
@@ -25,41 +25,74 @@ public:
     */
     TRMutex(); 
 
+    /**
+     * Priority levels. If indicator is on FATAL, will print statements only with FATAL indicator. 
+     * If indicator is on WARNING, will print statements with WARNING indicator or higher. 
+     * If indicator is on DEFAULT, will print statements with DEFAULT warning indicator or higher
+     * 
+     * Hierachy: FATAL --> WARNING --> DEFAULT
+     * 
+     * DEFAULT is the default mode.
+    */
+
+    enum priorityLevels {
+        FATAL = 1,
+        wARNING = 2,
+        DEFAULT = 3,
+    };
+
+    /**
+     * int storing priorityIndicator
+    */
+
+    priorityLevels priorityIndicator;
+
+    /**
+     * Method to change priority level to determine which print statements will happen
+     * @param enum of priority level desired
+    */
+
+    void updatePriority(priorityLevels desiredLevel);
 
     /**
      * Method to print integer arguments with no newline at the end.
      * @param integer to print 
+     * @param enum of print priority level
     */
-    void print(int integer);
+    void print(int integer, priorityLevels priority = DEFAULT);
 
 
     /**
      * Method to print string arguments with no newline at the end.
      * @param string to print
+     * @param enum of print priority level
     */
-    void print(char statement[]);
+    void print(char statement[], priorityLevels priority = DEFAULT);
 
     
     /**
      * Method to print integer arguments with a newline at the end.
      * @param integer to print
+     * @param enum of print priority level
     */
-    void println(int integer);
+    void println(int integer, priorityLevels priority = DEFAULT);
 
 
     /**
      * Method to print string arguments with a newline at the end.
      * @param string to print
+     * @param enum of print priority level
     */
-    void println(char statement[]);
+    void println(char statement[], priorityLevels priority = DEFAULT);
 
 
     /**
      * Method that mimics the printf() functionality
      * @param string that contains the formatting desired. (eg. %d, %s etc.) 
+     * @param enum of print priority level
      * @param arguments to be formatted  
     */
-    void printff(const char* format, ...);
+    void printff(const char* format, priorityLevels priority = DEFAULT, ...);
 
 
     /**
