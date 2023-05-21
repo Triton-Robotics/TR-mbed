3fc856a1c3a3790f7b0b7c829dc02f6499f27bda
GioByte10 <gvanni.bernal10@gmail.com>
2023-05-20 19:30:41 -0700

    Jaspdexer power optimizations

--- robots/TestBench/main.cpp
+++ robots/TestBench/main.cpp
@@ -15,19 +15,32 @@ DigitalOut led(LED1);
 int main(){
 
     unsigned long loopTimer = us_ticker_read() / 1000;
-    float pos = 85;
-
     bool previousMid;
     bool nowUp;
     bool nowDown;
 
     //remote.unfiltered = true;
     jaspdexer.justPosError = true;
-    jaspdexer.setPositionOutputCap(30000);
-    jaspdexer.setSpeedOutputCap(50000);
+    jaspdexer.setPositionOutputCap(100000);
+    jaspdexer.setSpeedOutputCap(1000000);
+    jaspdexer.setPositionIntegralCap(100000);
+    jaspdexer.setSpeedIntegralCap(100000);
+    jaspdexer.outCap = 100000;
 
     DJIMotor::setCANHandlers(&canHandler1,&canHandler2, false, false);
+    DJIMotor::sendValues();
     DJIMotor::getFeedback();
+
+    int actualPosition = jaspdexer.getData(MULTITURNANGLE);
+    int s = 0;
+    float desiredPosition = float(int((actualPosition / (8191.0 / 9.0))) * (8191.0 / 9.0)) + 100;
+
+    printf("------------------------------------\n");
+    printf("[i] %d\n", int(actualPosition));
+    printf("[p] %d\n", int(desiredPosition));
+
+    //ThisThread::sleep_for(5000ms);
+
 
     while (true) {
         unsigned long timeStart = us_ticker_read() / 1000;
@@ -42,26 +55,44 @@ int main(){
             nowDown = bool(remote.getSwitch(Remote::Switch::LEFT_SWITCH) == Remote::SwitchState::DOWN);
 
             led = !led;
+            actualPosition = jaspdexer.getData(MULTITURNANGLE);
 
-            if(rS == Remote::SwitchState::MID) {
+            if(rS == Remote::SwitchState::UP) {
                 if(previousMid && nowUp)
-                    pos += 8191.0 / 9;
+                    desiredPosition += 8191.0 / 9;
 
                 else if(previousMid && nowDown)
-                    pos -= 8191.0 / 9;
+                    desiredPosition -= 8191.0 / 9;
 
-                jaspdexer.setPosition(int(pos));
-                printf("POS: %d\n", int(pos));
+                jaspdexer.setPosition(int(desiredPosition));
 
             }else if(rS == Remote::SwitchState::DOWN) {
-                pos = 85;
-                jaspdexer.setPosition(int(pos));
-                printf("POS: %d\n", int(pos));
+                if(previousMid && nowUp)
+                    desiredPosition += 8191.0 / 9;
 
-            }else
+                else if(previousMid && nowDown)
+                    desiredPosition -= 8191.0 / 9;
+
+                s = 3000 * (int((desiredPosition - actualPosition) / (8191.0 / 9.0)) + 1);
+                if (s > 32760)
+                    s = 32760;
+
+                if(int(desiredPosition) > actualPosition)
+                    jaspdexer.setPower(s);
+
+                else
+                    jaspdexer.setPower(0);
+
+                printf("s %d\n", s);
+
+            }else if(rS == Remote::SwitchState::MID) {
+                desiredPosition = float(int((actualPosition / (8191.0 / 9.0))) * (8191.0 / 9.0)) + 100;
+                jaspdexer.setPosition(int(desiredPosition));
                 jaspdexer.setPower(0);
+            }
 
-            printf("POSITION: %d\n\n", jaspdexer.getData(MULTITURNANGLE));
+            printf("pos: %d\n", int(desiredPosition));
+            printf("POSITION: %d\n\n", actualPosition);
 
             DJIMotor::sendValues();
         }
