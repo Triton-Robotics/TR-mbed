411de2823b7c3dd7db33b78f266613d33da453db
GioByte10 <gvanni.bernal10@gmail.com>
2023-10-02 00:05:30 -0700

    Barebone code

--- robots/TestBench/main.cpp
+++ robots/TestBench/main.cpp
@@ -1,32 +1,28 @@
-#include <cstdlib>
-#include "mbed.h"
-#include "../core/util/peripherals/imu/BNO055.h"
+include "main.h"
 
-I2C    i2c(PB_9, PB_8);                // SDA, SCL
-BNO055 imu(i2c, PA_8, MODE_IMU);
-
-BNO055_ID_INF_TypeDef bno055_id_inf;
-BNO055_QUATERNION_TypeDef       quat;
-BNO055_ANGULAR_POSITION_typedef p;
+DigitalOut led(L27);
 
 int main(){
 
-    imu.set_mounting_position(MT_P1);
+    DJIMotor::s_setCANHandlers(&canHandler1, &canHandler2, false, false);
+    DJIMotor::s_sendValues();
+    DJIMotor::s_getFeedback();
 
-    printf("Bosch Sensortec BNO055 test program on " __DATE__ "/" __TIME__ "\n");
+    unsigned long timeStart;
+    unsigned long loopTimer = us_ticker_read();
 
-    if (imu.chip_ready() == 0)
-        printf("Bosch BNO055 is NOT available!!\r\n");
+    while(true){
+        timeStart = us_ticker_read();
 
-    imu.read_id_inf(&bno055_id_inf);
-    printf("CHIP:0x%02x, ACC:0x%02x, MAG:0x%02x, GYR:0x%02x, , SW:0x%04x, , BL:0x%02x\n",
-           bno055_id_inf.chip_id, bno055_id_inf.acc_id, bno055_id_inf.mag_id,
-           bno055_id_inf.gyr_id, bno055_id_inf.sw_rev_id, bno055_id_inf.bootldr_rev_id);
+        if ((timeStart - loopTimer) / 1000 > 25){
+            led = !led;
 
-    while (true) {
-        imu.get_angular_position_quat(&p);
-        printf("Heading:%d [deg], Roll:%d [deg], Pitch:%d [deg]\n", (int)p.yaw, (int)p.roll, (int)p.pitch);
+            remoteRead();
 
+            loopTimer = timeStart;
+            DJIMotor::s_sendValues();
+        }
+        DJIMotor::s_getFeedback();
+        ThisThread::sleep_for(1ms);
     }
 }
-
