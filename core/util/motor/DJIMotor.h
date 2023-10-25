68d5ad0f96c69dd35e8c173ffce344504cd3ba39
Giovanni Bernal Ramirez <gvanni.bernal10@gmail.com>
2023-10-25 12:32:16 -0700

    Update DJIMotor.h to new remote read way

--- core/util/motor/DJIMotor.h
+++ core/util/motor/DJIMotor.h
@@ -1,43 +1,11 @@
-#ifndef TR_EMBEDDED_DJIMOTOR_H
-#define TR_EMBEDDED_DJIMOTOR_H
-
-#include "mbed.h"
-#include "algorithms/PID.h"
-#include "communications/CANHandler.h"
-#include <cmath>
 #include <string>
-
-constexpr double M3508_GEAR_RATIO = 3591.0 / 187.0;
-constexpr double M2006_GEAR_RATIO = 36.0;
-
-constexpr int TICKS_REVOLUTION = 8192;
-constexpr int TIMEOUT_MS = 400;
-constexpr int CAN_HANDLER_NUMBER = 2;
-
-constexpr int INT16_T_MAX = 32767;
-constexpr int INT15_T_MAX = 16384;
-
-static int s_sendIDs[3] = {0x200, 0x1FF, 0x2FF};           //IDs to send data
-static Thread s_motorFeedbackThread(osPriorityAboveNormal);     //threading for Motor::tick()
-static Thread s_motorSendThread(osPriorityNormal);              //threading for Motor::tick()
-
-enum motorDataType {
-    ANGLE,
-    VELOCITY,
-    TORQUE,
-    TEMPERATURE,
-    MULTITURNANGLE,
-    POWEROUT
-};
-
-
-//keep in mind that in the constructor, this is only used to
-//set the default pid values and gear ratio. The motorType will
-//be changed to STANDARD, because that's what the code uses.
+#include <cstdint>
+#include <cstdio>
+const int CAN_HANDLER_NUMBER = 2;
 
 enum motorType {
     NONE = 0,
-    STANDARD = 1,       //identifier for all motors that use the standard can protocol, used by the C610 and C620
+    STANDARD = 1,
 
     GIMBLY = 2,
     GM6020 = 2,
@@ -50,39 +18,45 @@ enum motorType {
     M3508_FLYWHEEL = 5,
 };
 
-class DJIMotor {
+enum motorDataType {
+    ANGLE,
+    VELOCITY,
+    TORQUE,
+    TEMPERATURE,
+    POWER,
+};
+
+class Remote {
+public:
+    enum class SwitchState
+    {
+        UNKNOWN,
+        DOWN,
+        MID,
+        UP
+    };
+};
+
+// never use global variables, we are using them here due to the nature of this assignment
+Remote::SwitchState lS;         // left switch
+int lX;                         // x value of left joystick
+
+class CANHandler{
+public:
+    enum CANBus {CANBUS_1, CANBUS_2, NOBUS};
+};
+
+class DJIMotor{
 
 private:
+    static DJIMotor* s_allMotors  [CAN_HANDLER_NUMBER][3][4];
+    static bool s_motorsExist     [CAN_HANDLER_NUMBER][3][4];
 
-    struct PidSettings{
-        float p;
-        float i;
-        float d;
-        float outCap;
-        float integralCap;
-    };
-
-    struct MotorSettings{
-        PidSettings pos;
-        PidSettings speed;
-    };
-
-    // Default motor PID settings
-
-    MotorSettings gimbly = {
-            {10.88,1.2,18.9,8000,500},
-            {0.13, 8.8, 0, 25000, 1000}
-    };
-
-    MotorSettings m3508 = {
-            {.48, 0.0137, 4.2, 3000, 300},
-            {1.79, 0.27, 10.57, 15000, 500}
-    };
-
-    MotorSettings m3508Flywheel = {
-            {.48, 0.0137, 4.2, 3000, 300},
-            {2.013, 0.319, 20.084, 15000, 500}
-    };
+    short motorID_0;
+    short canID_0;
+    motorType type;
+    CANHandler::CANBus canBus;
+    std::string name;
 
     enum motorMoveMode{
         OFF,
@@ -92,99 +66,119 @@ private:
         ERR
     };
 
-    static DJIMotor* s_allMotors  [CAN_HANDLER_NUMBER][3][4];
-    static bool s_motorsExist     [CAN_HANDLER_NUMBER][3][4];
-
-    static CANHandler* s_canHandlers[CAN_HANDLER_NUMBER];
-    CANHandler::CANBus canBus = CANHandler::NOBUS;
-
-    short canID_0;                                                  // canID - 1, because canID is 1-8, arrays are 0-7
-    short motorID_0;                                                // physical motorID - 1
-    motorType type = NONE;                                          // type of the motor
-    motorMoveMode mode = OFF;                                       // mode of the motor
-
-    //  angle | velocity | torque | temperature
-    int16_t motorData[4] = {};
     int value = 0;
+    motorMoveMode mode = OFF;
 
-    int lastMotorAngle = 0;
-    int integratedAngle = 0;
-    long lastIntegrationTime = -1;
-
-    uint32_t timeOfLastFeedback = 0;
-    uint32_t timeOfLastPID = 0;
-
-    static void s_updateMultiTurnPosition();
-    static void s_sendOneID(CANHandler::CANBus canBus, short sendIDindex, bool debug = false);
-    static int s_calculateDeltaTicks(int target, int current);
-
-    void setOutput();
+    int16_t motorData[5] = {};
 
 public:
+    DJIMotor(short motorID, CANHandler::CANBus canBus, motorType type, const std::string& name) {
+        canID_0 = static_cast<short>(motorID - 1);
+        motorID_0 = canID_0;
+        this -> canBus = canBus;
+        this -> type = type;
+        this -> name = name;
 
-    static int motorCount;
-    std::string name = "NO_NAME";
-    int16_t powerOut = 0;
+        if(type == GM6020)
+            canID_0 += 4;
 
-    PID pidSpeed;
-    PID pidPosition;
+        s_allMotors[canBus][canID_0 / 4][canID_0 % 4] = this;
+        s_motorsExist[canBus][canID_0 / 4][canID_0 % 4] = true;
+    }
 
-    int multiTurn = 0;
-    int outputCap = INT16_T_MAX;
-    bool useAbsEncoder = false;
-    bool printAngle = false;
-
-    explicit DJIMotor(bool isErroneousMotor = false);
-    DJIMotor(short motorID, CANHandler::CANBus canBus, motorType type = STANDARD, const std::string& name = "NO_NAME");
-    ~DJIMotor();
-
-    static void s_setCANHandlers(CANHandler* bus_1, CANHandler* bus_2, bool threadSend = true, bool threadFeedback = true);
-    static void s_getFeedback(bool debug = false);
-    static void s_sendValues(bool debug = false);
-    static void s_feedbackThread();
-    static void s_sendThread();
-
-    __attribute__((unused)) static bool s_allMotorsConnected(bool debug = false);
-
-    int getData(motorDataType data);
-    void printAllMotorData();
-
-    inline void setPower(int power){
-        value = power;
+    void setPower(int power){
         mode = POW;
+        value = power;
     }
 
-    inline void setSpeed(int speed){
-        value = speed;
+    void setSpeed(int speed){
         mode = SPD;
+        value = speed;
     }
 
-    inline void setPosition(int position){
-        value = position;
+    void setPosition(int position){
         mode = POS;
+        value = position;
     }
 
-    __attribute__((unused)) inline bool isConnected() const {
-        return us_ticker_read() / 1000 - timeOfLastFeedback <= TIMEOUT_MS;
+    void setOutput(){
+        if(mode == POW)
+            motorData[POWER] = static_cast<int16_t>(value);
+
+        else if(mode == SPD)
+            motorData[VELOCITY] = static_cast<int16_t>(value);
+
+        else if(mode == POS)
+            motorData[ANGLE] = static_cast<int16_t>(value);
     }
-    inline static bool s_isMotorConnected(CANHandler::CANBus bus, motorType type, short canID){
-        return (s_motorsExist[bus][type][canID] && us_ticker_read() / 1000 - s_allMotors[bus][type][canID] -> timeOfLastFeedback <= TIMEOUT_MS);
+
+    static void s_sendValues(){
+        for(int canBus = 0; canBus < CAN_HANDLER_NUMBER; canBus ++)
+            for(int j = 0; j < 3; j ++)
+                for(int k = 0; k < 4; k ++)
+                    if(s_motorsExist[canBus][j][k])
+                        s_allMotors[canBus][j][k] -> setOutput();
+
     }
 
-    inline int operator>>(motorDataType data)                                   { return getData(data); }
+    static void s_getFeedback(){
+        // need for PID in real class
+    }
 
-    inline void setPositionPID(float kP, float kI, float kD)                    { pidPosition.setPID(kP, kI, kD); }
-    inline void setPositionIntegralCap(double cap)                              { pidPosition.setIntegralCap((float)cap); }
-    inline void setPositionOutputCap(double cap)                                { pidPosition.setOutputCap((float)cap); }
-
-    inline void setSpeedPID(float kP, float kI, float kD)                       { pidSpeed.setPID(kP, kI, kD); }
-    inline void setSpeedIntegralCap(double cap)                                 { pidSpeed.setIntegralCap((float)cap); }
-    inline void setSpeedOutputCap(double cap)                                   { pidSpeed.setOutputCap((float)cap); }
-
-    inline int calculateSpeedPID(int desired, int current, double dt)           { return pidSpeed.calculate(desired, current, dt); }
-    inline int calculatePositionPID(int desired, int current, double dt)        { return pidPosition.calculate(desired, current, dt); }
-    inline int calculatePeriodicPosition(float dE, double dt)                   { return pidPosition.calculatePeriodic(dE, dt); }
-
+    int getData(motorDataType data){
+        return motorData[data];
+    }
 };
 
-#endif //TR_EMBEDDED_DJIMOTOR_H
+DJIMotor* DJIMotor::s_allMotors[2][3][4];
+bool DJIMotor::s_motorsExist[2][3][4];
+
+struct remote_{
+private:
+    Remote::SwitchState lS;
+    int lX;
+
+public:
+    Remote::SwitchState leftSwitch() const{
+        return lS;
+    }
+
+    int leftX() const{
+        return lX;
+    }
+
+    void read(bool debug = false){
+        static int count;
+        count %= 660;
+
+        if((count % 15) / 5  == 0)
+            lS = Remote::SwitchState::UP;
+
+        else if((count % 15) / 5 == 1)
+            lS = Remote::SwitchState::MID;
+
+        else if((count % 15) / 5 == 2)
+            lS = Remote::SwitchState::DOWN;
+
+        lX = count;
+
+        if(debug){
+            printf("lX:[%d] lS:[%d]\n",lX, (int)lS);
+        }
+
+        count++;
+    }
+} remote;
+
+///////////////
+//main.cpp
+///////////////
+
+// ONLY ADD CODE BELOW
+// Use prints and getData() for debugging purposes
+
+int main(){
+
+
+    
+}
