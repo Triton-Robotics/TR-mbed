b3dbf405e4294e2e1b1d354e11432dde3279c901
GioByte10 <gvanni.bernal10@gmail.com>
2024-03-02 15:48:28 -0800

    updated tuner

--- core/util/algorithms/PIDTuner.cpp
+++ core/util/algorithms/PIDTuner.cpp
@@ -1,85 +1,94 @@
 #include "PIDTuner.h"
 
-PIDTuner::PIDTuner(DJIMotor &motor_) : motor{motor_}{
+PIDTuner::PIDTuner(DJIMotor &motor_, PIDDataSet population[], size_t len, PIDRanges ranges) : motor{motor_}{
     motor.useAbsEncoder = true;
+
+    for(int i; i < len; i++){
+        population[i].kP = PIDTuner::getRandomNumber(ranges.kPMin, ranges.kPMax);
+        population[i].kI = PIDTuner::getRandomNumber(ranges.kIMin, ranges.kIMax);
+        population[i].kD = PIDTuner::getRandomNumber(ranges.kDMin, ranges.kDMax);
+    }
 }
 
-void PIDTuner::tune(PIDDataSet dataSet[], int len, unsigned long time_s, int iterations){
-    kP = 1;
-    kI = 0;
-    kD = 0;
-
+void PIDTuner::tune(PIDDataSet population[], size_t len, unsigned long time_s, size_t iterations, size_t generations){
     printf("tuning %s...\n", motor.name.c_str());
 
-    for(int i = 0; i < len; i++){
-        motor.pidPosition.resetPID(kP, kI, kD);
+    for(int generation = 0; generation < generations; generation++) {
+        for (int i = 0; i < len; i++) {
+            motor.pidPosition.resetPID(population[i].kP, population[i].kI, population[i].kD);
 
-        dataSet[i].kP = kP;
-        dataSet[i].kI = kI;
-        dataSet[i].kD = kD;
+            DJIMotor::s_sendValues();
+            DJIMotor::s_getFeedback();
 
-        DJIMotor::s_sendValues();
-        DJIMotor::s_getFeedback();
+            double lastError_ = DJIMotor::s_calculateDeltaPhase(0, motor.getData(ANGLE));
+            double integralAbsErrorSum_ = 0;
+            double integralSqErrorSum_ = 0;
 
-        dataSet[i].startError = DJIMotor::s_calculateDeltaPhase(0, motor.getData(ANGLE));
-        dataSet[i].maxError = dataSet[i].startError;
-        dataSet[i].minError = dataSet[i].startError;
+            for (int j = 0; j < iterations; j++) {
 
-        double lastError_ = dataSet[i].startError;
-        double integralAbsErrorSum_ = 0;
-        double integralSqErrorSum_ = 0;
+                unsigned long timeStart;
+                unsigned long loopTimer = us_ticker_read();
+                unsigned long start_u = us_ticker_read();
+                unsigned long current_u = us_ticker_read();
 
-        for(int j = 0; j < iterations; j++) {
+                while ((current_u - start_u) / 1000 < (time_s) * 1000) {
+                    timeStart = us_ticker_read();
 
-            unsigned long timeStart;
-            unsigned long loopTimer = us_ticker_read();
-            unsigned long start_u = us_ticker_read();
-            unsigned long current_u = us_ticker_read();
+                    if ((timeStart - loopTimer) / 1000 > 25) {
 
-            while ((current_u - start_u) / 1000 < (time_s) * 1000) {
-                timeStart = us_ticker_read();
+                        // -----------------------------------------
+                        motor.setPosition(0);
 
-                if ((timeStart - loopTimer) / 1000 > 25) {
-
-                    // -----------------------------------------
-                    motor.setPosition(0);
-
-                    double error = DJIMotor::s_calculateDeltaPhase(0, motor.getData(ANGLE));
-                    double dt_m = static_cast<double>(us_ticker_read() - current_u) / 1000;
+                        double error = DJIMotor::s_calculateDeltaPhase(0, motor.getData(ANGLE));
+                        double dt_m = static_cast<double>(us_ticker_read() - current_u) / 1000;
 
 
-                    integralAbsErrorSum_ += 0.01 * abs(dt_m * (error + lastError_) / 2);
-                    integralSqErrorSum_ += 0.01 * dt_m * pow(((error + lastError_) / 2), 2);
+                        integralAbsErrorSum_ += 0.01 * abs(dt_m * (error + lastError_) / 2);
+                        integralSqErrorSum_ += 0.01 * dt_m * pow(((error + lastError_) / 2), 2);
 
-                    lastError_ = error;
+                        lastError_ = error;
 
-                    current_u = us_ticker_read();
-                    // -----------------------------------------
+                        current_u = us_ticker_read();
+                        // -----------------------------------------
 
-                    loopTimer = timeStart;
-                    DJIMotor::s_sendValues();
+                        loopTimer = timeStart;
+                        DJIMotor::s_sendValues();
+                    }
+                    DJIMotor::s_getFeedback();
+                    ThisThread::sleep_for(1ms);
                 }
-                DJIMotor::s_getFeedback();
-                ThisThread::sleep_for(1ms);
+                randomizePosition();
+                motor.pidPosition.resetErrorIntegral();
             }
-
-            int power = 2 * static_cast<int>((static_cast<float>(rand() - RAND_MAX / 2) / RAND_MAX) * static_cast<float>(motor.type == GIMBLY ? INT16_T_MAX : INT15_T_MAX));
-
-            printf("power: %d %d\n", power, RAND_MAX);
-            motor.setPower(power);
-            DJIMotor::s_sendValues();
-            ThisThread::sleep_for(250ms);
-
-            motor.setPower(0);
-            DJIMotor::s_sendValues();
-            ThisThread::sleep_for(700ms);
+            population[i].meanIntegralAbsError = integralAbsErrorSum_ / iterations;
+            population[i].meanIntegralSqError = integralSqErrorSum_ / iterations;
         }
-
-        dataSet[i].meanIntegralAbsError = integralAbsErrorSum_ / iterations;
-        dataSet[i].meanIntegralSqError = integralSqErrorSum_ / iterations;
-
-        kP += 10;
-        kI += 0.1;
-        kD += 10;
+        // mating
     }
+}
+
+float PIDTuner::getRandomNumber(float min, float max) {
+    static std::random_device device;
+    static std::mt19937 mt(device());
+    static std::uniform_real_distribution<float> dist(min, max);
+
+    return dist(mt);
+}
+
+void PIDTuner::randomizePosition() {
+    float max = static_cast<float>(motor.type == GIMBLY ? INT16_T_MAX : INT15_T_MAX);
+    int power = static_cast<int>(PIDTuner::getRandomNumber(-max, max));
+
+    printf("power: %d %d\n", power, RAND_MAX);
+    motor.setPower(power);
+    DJIMotor::s_sendValues();
+    ThisThread::sleep_for(250ms);
+
+    motor.setPower(0);
+    DJIMotor::s_sendValues();
+    ThisThread::sleep_for(700ms);
+}
+
+void PIDTuner::crossover(bool sexual) {
+
 }
--- core/util/algorithms/PIDTuner.h
+++ core/util/algorithms/PIDTuner.h
@@ -3,38 +3,44 @@
 #include "../motor/DJIMotor.h"
 #include <random>
 
+using fitnessFunction = std::function<double(double)>;
+
 struct PIDDataSet{
     float kP = 0;
     float kI = 0;
     float kD = 0;
 
-    int startError = 0;
-    int maxError = 0;
-    int minError = 0;
-
     double meanIntegralAbsError = 0;
     double meanIntegralSqError = 0;
 
-    void dumpData(){
+    void dumpData() const{
         printf("kP: %f kI: %f kD: %f\n", kP, kI, kD);
-        printf("Error start: %d max: %d min: %d\n", startError, maxError, minError);
         printf("IAE: %lf ISE: %lf\n\n", meanIntegralAbsError, meanIntegralSqError);
     }
 };
 
+struct PIDRanges{
+    float kPMin;
+    float kPMax;
+    float kIMin;
+    float kIMax;
+    float kDMin;
+    float kDMax;
+};
+
 class PIDTuner {
 public:
-    PIDTuner(DJIMotor &motor_);
-    void tune(PIDDataSet dataSet[], int len, unsigned long time_s, int iterations);
+    PIDTuner(DJIMotor &motor_, PIDDataSet population[], size_t len, PIDRanges ranges = {0, 30, 0, 10, 0, 1000});
+    void tune(PIDDataSet population[], size_t len, unsigned long time_s, size_t iterations, size_t generations);
 
     ~PIDTuner() = default;
 
 private:
     DJIMotor &motor;
 
-    float kP;
-    float kI;
-    float kD;
+    static float getRandomNumber(float min, float max);
+    void randomizePosition();
+    void crossover(bool sexual = true);
 
 };
 
