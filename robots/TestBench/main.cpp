5cc901c6c10ef3508bcc30efe1caf4427005e4c1
Anshal Jain <pk.ness.ness@gmail.com>
2024-06-16 21:48:27 -0700

    b4 i try funky FF for speed velo

--- robots/TestBench/main.cpp
+++ robots/TestBench/main.cpp
@@ -5,7 +5,10 @@ DigitalOut led2(L26);
 DigitalOut led3(L25);
 DigitalOut ledbuiltin(LED1);
 
-DJIMotor testMot(4, CANHandler::CANBUS_1, M3508, "testbench_motor");
+DJIMotor testMot(5, CANHandler::CANBUS_1, GM6020, "testbench_motor");
+
+#define IMPULSE_DT 100
+#define IMPULSE_STRENGTH 16383
 
 int main(){
 
@@ -17,7 +20,12 @@ int main(){
     unsigned long loopTimer = us_ticker_read();
     int refLoop = 0;
 
-    testMot.setSpeedPID(1.5,0,0);
+    testMot.setSpeedPID(0.5,0,0);
+
+    bool prevL = false;
+    bool switL = false;
+
+    int motorSpeed = 0;
 
     while(true){
         timeStart = us_ticker_read();
@@ -32,13 +40,28 @@ int main(){
                 refereeThread(&referee);
                 refLoop = 0;
                 led2 = !led2;
-                printff("datum:%d %d %d\n", testMot>>ANGLE, testMot>>VELOCITY, remote.leftX());
             }
-
+            prevL = switL;
             remoteRead();
+            switL = (remote.leftSwitch() == Remote::SwitchState::UP);
 
-            testMot.setPower(remote.leftX() * 3);
-            
+            // testMot.setPower(remote.leftX() * 3);
+            // if(!prevL && switL){
+            //     motorSpeed += 10;
+            // }
+
+            motorSpeed = remote.leftX() / 6;
+
+            testMot.setSpeed(motorSpeed);
+            int dir = 0;
+            if(motorSpeed > 0){
+                dir = 1;
+            }else if(motorSpeed < 0){
+                dir = -1;
+            }
+            testMot.pidSpeed.feedForward = dir * (874 + (73.7 * abs(motorSpeed)) + (0.0948 * motorSpeed * motorSpeed));
+            printff("%d\t%d\t%d\n", testMot>>POWEROUT, motorSpeed, testMot>>VELOCITY);
+
             DJIMotor::s_sendValues();
         }
         DJIMotor::s_getFeedback();
