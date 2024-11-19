0d99472c61df1596e297e5f44b037b0dc95ca673
Anshal Jain <pk.ness.ness@gmail.com>
2024-11-18 21:38:50 -0800

    imu raw

--- robots/TestBench/main.cpp
+++ robots/TestBench/main.cpp
@@ -8,6 +8,8 @@ DigitalOut ledbuiltin(LED1);
 #define IMPULSE_DT 100
 #define IMPULSE_STRENGTH 16383
 
+I2C i2c(PB_7, PB_8);
+
 int main(){
 
     DJIMotor::s_setCANHandlers(&canHandler1, &canHandler2, false, false);
@@ -18,13 +20,17 @@ int main(){
     unsigned long loopTimer = us_ticker_read();
     int refLoop = 0;
 
-    bool prevL = false;
-    bool switL = false;
-
     int motorSpeed = 0;
 
     while(true){
         timeStart = us_ticker_read();
+
+        // char opr[2] = {0x3F, 0x20};
+        char opr[2] = {0x3F, 0x20};
+        i2c.write(0x29 << 1, opr, 2, true);
+        
+        char opr[2] = {0x3B, 0x0C};
+        i2c.write(0x29 << 1, opr, 2, true);
 
         if ((timeStart - loopTimer) / 1000 > 25){
             loopTimer = timeStart;
@@ -39,7 +45,20 @@ int main(){
             }
             remoteRead();
             
-            
+            //i2c
+
+            int len = 32;
+            char data[len] = {0};
+            data[0] = 0x08;
+            i2c.write(0x29 << 1, data, 1, true);
+            ThisThread::sleep_for(10ms);
+            i2c.read(0x29 << 1, data, len, false);
+
+            //printff("%d %d : %x %x\n",w,r,data[0], data[1]);
+            for(int i = 0; i < len; i += 2){
+                printff("%x %x %.1f ", data[i], data[i+1], float(data[i] | (data[i + 1] << 8)));
+            }
+            printff("\n");
 
             DJIMotor::s_sendValues();
         }
