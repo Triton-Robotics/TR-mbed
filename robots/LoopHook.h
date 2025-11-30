404a611f18b7990254fdf0bd3be301491ecb5527
LuminousLlama <keyushattarde@gmail.com>
2025-11-30 02:18:38 -0800

    comment

--- robots/LoopHook.h
+++ robots/LoopHook.h
@@ -7,6 +7,11 @@ namespace TR
     struct look_hooks
     {
         std::function<void()> init;
+
+        // Instead of having a callback for each subsystem we should just have
+        // 1. init
+        // 2. post IO (guarantees ref and comms/jetson reads are completed)
+        // 3. end of loop (for printing/debugging etc. )
         std::function<void()> turret_executor;
         std::function<void()> chassis_executor;
         std::function<void()> shooter_executor;
