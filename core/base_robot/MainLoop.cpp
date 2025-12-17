e7e20b771ee9579de16d88784922563428fa8456
Arihant Jain <arihantjaingoa@gmail.com>
2025-12-18 02:51:30 +0300

    comments to mainLoop explaining sections

--- core/base_robot/MainLoop.cpp
+++ core/base_robot/MainLoop.cpp
@@ -8,16 +8,23 @@ void BaseRobot::main_loop()
 
     unsigned long main_loop_dt_ms = 1; // idk how to call main_loop_dt_ms() without erroring for some reason
 
+    // Init all constants, subsystems, sensors, IO, etc.
     init();
 
     while (true)
     {
+        // TODO: StmIO comms (Ref and Jetson)
+
         loop_clock_us = us_ticker_read();
         if ((loop_clock_us - prev_loop_time_us) / 1000 >= main_loop_dt_ms)
         {
+            // Add subsystems in periodic
             periodic(loop_clock_us - prev_loop_time_us);
             prev_loop_time_us = us_ticker_read();
         }
+        // Add sensors updates in your end of loop
         end_of_loop();
+
+        // TODO: Motor updates
     }
 }
