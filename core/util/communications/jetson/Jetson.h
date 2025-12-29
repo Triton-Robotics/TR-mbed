4bd740a2ababe3a71aeed421d3cda9317cf86ea5
LuminousLlama <keyushattarde@gmail.com>
2025-12-28 22:52:15 -0800

    delete outdated comment

--- core/util/communications/jetson/Jetson.h
+++ core/util/communications/jetson/Jetson.h
@@ -68,22 +68,6 @@ class Jetson {
     static constexpr unsigned long WRITE_THREAD_LOOP_DT_MS = 1;
 };
 
-// we want to be able to send both struct types, so we could have 4 args:
-// bcJetson, &jetson_send_data = nullptr, &jetson_send_ref = nullptr, ref_data =
-// False void jetson_send_feedback(BufferedSerial &bcJetson, const
-// Jetson_send_ref& ref_data, const Jetson_send_data& data, int msg_type = 0);
-
-// same w receiving
-// bcJetson, &jetson_read_data = nullptr, &jetson_read_new_odom = nullptr
-// here we return num bytes to determine which type? or we can give a bool to
-// know which one is updated well either way we look at both :D ssize_t
-// jetson_read_values(BufferedSerial &bcJetson, Jetson_read_data& read_data,
-// Jetson_read_odom& odom_data);
-
-// spi bs
-// ssize_t jetson_send_read_spi(SPI &spiJetson, const Jetson_send_data& input,
-// Jetson_read_data& output);
-
 // ------
 // Write packets
 // ------
