89132c69d2b8a2539b2ebd7c1c825d2c628b5456
Moksh-Paniker <moksh@panikers.com>
2026-01-08 13:50:21 -0800

    Updated PID tools for one while loop

--- core/util/algorithms/PIDTools.cpp
+++ core/util/algorithms/PIDTools.cpp
@@ -12,7 +12,7 @@
 PIDTools::PIDTools(DJIMotor* motor, int des_val, TYPE type)
     :motor(motor), des_val(des_val), type(type) {}
 
-void PIDTools::test_velocity()
+ /*void PIDTools::test_velocity()
 {
     motor->setSpeed(des_val);
     // continue
@@ -25,8 +25,38 @@ void PIDTools::test_velocity()
     std::cout << "Rise Time: " << rise_time << "seconds" << std::endl;
     std::cout << "Settling Time: " << settling_time << "seconds" << std::endl;
 
+} */
+
+void PIDTools::test_velocity()
+{
+    if (running)
+    {
+        if (calc_overshoot)
+        {
+            calculateOvershootVelocity();
+        }
+        if (calc_settling_time)
+        {
+            calculateSettlingTimeVelocity();
+        }
+    }
 }
 
+void PIDTools::test_position()
+{
+    if (running)
+    {
+        if (calc_overshoot)
+        {
+            calculateOvershootVelocity();
+        }
+        if (calc_settling_time)
+        {
+            calculateSettlingTimeVelocity();
+        }
+    }
+}
+/*
 void PIDTools::test_position()
 {
     motor->setPosition(des_val);
@@ -41,12 +71,7 @@ void PIDTools::test_position()
     std::cout << "Settling Time: " << settling_time << "seconds" << std::endl;
 
 }
-
-void printResults(float overshoot, float rise_time, float settling_time)
-{
-
-}
-
+*/
 std::string PIDTools::run()
 {
     if (type == TYPE::VELOCITY)
@@ -60,6 +85,108 @@ std::string PIDTools::run()
     return "Testing complete";
 }
 
+
+void PIDTools::calculateOvershootVelocity()
+{
+    start_time = us_ticker_read();
+    current_value = motor->getData(VELOCITY);
+    if (abs(current_value) >= abs(max_value))
+    {
+        max_value = current_value;
+    }
+
+    if (current_value >= des_val && calc_rise_time)
+    {
+        passed_des = true;
+        end_time = us_ticker_read();
+        rise_time = end_time - start_time;
+        calc_rise_time = false;
+    }
+
+    if (abs(current_value) < abs(max_value) && passed_des)
+    {
+        overshoot = abs(max_value - des_val);
+        calc_overshoot = false;
+    }
+}
+
+void PIDTools::calculateOvershootPosition()
+{
+    start_time = us_ticker_read();
+    current_value = motor->getData(VELOCITY);
+    current_value -= initial_value;
+
+    if ((current_value) > 8192)
+    {
+        current_value -= 8192;
+    }
+    else if (current_value < 0)
+    {
+        current_value += 8192;
+    }
+
+    if (abs(current_value) >= abs(max_value))
+    {
+        max_value = current_value;
+    }
+
+    if (abs(current_value) >= abs(max_value))
+    {
+        max_value = current_value;
+    }
+
+    if (current_value >= des_val && calc_rise_time)
+    {
+        passed_des = true;
+        end_time = us_ticker_read();
+        rise_time = end_time - start_time;
+        calc_rise_time = false;
+    }
+
+    if (abs(current_value) < abs(max_value) && passed_des)
+    {
+        calc_overshoot = false;
+        overshoot = abs(max_value - des_val);
+    }
+}
+
+void PIDTools::calculateSettlingTimeVelocity()
+{
+    start_settle_time = us_ticker_read();
+    time_in_range = us_ticker_read();
+    float current_value = motor->getData(VELOCITY);
+    auto now = us_ticker_read();
+    if (!(current_value >= lower_bound && current_value <= upper_bound))
+    {
+        time_in_range = us_ticker_read();
+    }
+
+    if (now - time_in_range >= 1000000 && calc_settling_time)
+    {
+        settling_time = now-start_time;
+        calc_settling_time = false;
+    }
+
+}
+
+void PIDTools::calculateSettlingTimePosition()
+{
+
+    float current_value = motor->getData(ANGLE) - initial_value;
+    auto now = us_ticker_read();
+    if (!(current_value >= lower_bound && current_value <= upper_bound))
+    {
+        time_in_range = us_ticker_read();
+    }
+
+    if (now - time_in_range >= 1000000 && calc_settling_time)
+    {
+        settling_time = now-start_time;
+        calc_settling_time = false;
+    }
+}
+
+/*
 float PIDTools::calculateOvershootVelocity(int des_val)
 {
     float current_value = motor->getData(VELOCITY);
@@ -91,7 +218,7 @@ float PIDTools::calculateOvershootVelocity(int des_val)
     return overshoot;
 }
 
-float PIDTools::calculateOvershootPosition(int des_val)
+void PIDTools::calculateOvershootPosition(int des_val)
 {
     float current_value = motor->getData(ANGLE);
     float initial_value = motor->getData(ANGLE);
@@ -100,9 +227,6 @@ float PIDTools::calculateOvershootPosition(int des_val)
 
     while (1)
     {
-
-        current_value = motor->getData(ANGLE);
-        current_value -= initial_value;
 
         if ((current_value) > 8192)
         {
@@ -131,6 +255,9 @@ float PIDTools::calculateOvershootPosition(int des_val)
 
     }
     float overshoot = abs(max_value - des_val);
+        current_value = motor->getData(ANGLE);
+        current_value -= initial_value;
+
     return overshoot;
 }
 
@@ -170,8 +297,7 @@ unsigned long PIDTools::calculateRiseTimePosition(int des_val)
 unsigned long PIDTools::calculateSettlingTimeVelocity(int des_val)
 {
     int initial_value = motor->getData(VELOCITY);
-    int lower_bound = 0.98 * (des_val - initial_value);
-    int upper_bound = 1.02 * (des_val - initial_value);
+
     auto start_time = us_ticker_read();
     auto time_in_range = us_ticker_read();
 
@@ -217,4 +343,4 @@ unsigned long PIDTools::calculateSettlingTimePosition(int des_val)
     }
 }
 
-
+*/
--- core/util/algorithms/PIDTools.h
+++ core/util/algorithms/PIDTools.h
@@ -5,6 +5,7 @@
 #ifndef TR_EMBEDDED_PIDTOOLS_H
 #define TR_EMBEDDED_PIDTOOLS_H
 #include "motor/DJIMotor.h"
+#include "util/motor/DJIMotor.h"
 
 enum class TYPE
 {
@@ -18,22 +19,65 @@ class PIDTools
     // constructor
     PIDTools(DJIMotor *motor, int des_val, TYPE type = TYPE::VELOCITY);
 
+    void init()
+    {
+        if (type == TYPE::VELOCITY)
+        {
+            motor->setSpeed(des_val);
+            current_value = motor->getData(VELOCITY);
+            max_value = current_value;
+        }
+        else
+        {
+            motor->setPosition(des_val);
+            current_value = motor->getData(ANGLE);
+            initial_value = motor->getData(ANGLE);
+            max_value = current_value;
+        }
+    }
     void test_velocity();
     void test_position();
     std::string run();
+
 
     private:
     DJIMotor *motor;
     int des_val;
     TYPE type;
+    float current_value;
+    float max_value;
+    static float initial_value;
 
-    float calculateOvershootPosition(int des_val);
-    unsigned long calculateRiseTimePosition(int des_val);
-    unsigned long calculateSettlingTimePosition(int des_val);
+    float overshoot;
+    unsigned long rise_time;
+    unsigned long settling_time;
 
-    float calculateOvershootVelocity(int des_val);
-    unsigned long calculateRiseTimeVelocity(int des_val);
-    unsigned long calculateSettlingTimeVelocity(int des_val);
+    bool running = true;
+    bool calc_overshoot = true;
+    bool calc_rise_time = true;
+    bool calc_settling_time = true;
+    bool passed_des = false;
+    static uint32_t start_time;
+    uint32_t end_time;
+    static uint32_t start_settle_time;
+    int lower_bound = 0.98 * (des_val - initial_value);
+    int upper_bound = 1.02 * (des_val - initial_value);
+    static uint32_t time_in_range;
+
+    std::string printData();
+
+    void calculateOvershootVelocity();
+    void calculateOvershootPosition();
+    void calculateSettlingTimeVelocity();
+    void calculateSettlingTimePosition();
+
+  //  float calculateOvershootPosition(int des_val);
+  //  unsigned long calculateRiseTimePosition(int des_val);
+  //  unsigned long calculateSettlingTimePosition(int des_val);
+
+  //  float calculateOvershootVelocity(int des_val);
+  //  unsigned long calculateRiseTimeVelocity(int des_val);
+  //  unsigned long calculateSettlingTimeVelocity(int des_val);
 
 };
 
