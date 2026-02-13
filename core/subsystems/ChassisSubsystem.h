72b65dda2f3ee0b71bddf80c64697992a91df552
Edward <chl244@ucsd.edu>
2026-02-12 17:02:26 -0800

    added encoder stuff

--- core/subsystems/ChassisSubsystem.h
+++ core/subsystems/ChassisSubsystem.h
@@ -171,7 +171,7 @@ public:
      *
      * @param desiredChassisSpeeds The robot-relative speeds (vX, vY, and vOmega) in m/s
      */
-    float setChassisSpeeds(ChassisSpeeds desiredChassisSpeeds_, DRIVE_MODE mode = ROBOT_ORIENTED);
+    float setChassisSpeeds(ChassisSpeeds desiredChassisSpeeds_, float encoder, DRIVE_MODE mode = ROBOT_ORIENTED);
 
     /**
      * The rotateChassisSpeed method
