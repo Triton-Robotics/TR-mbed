a5f6544b404cc38bf7132a6e829f8ad445711f08
Rong Shen Ng <ngrongshen2005@gmail.com>
2026-02-19 17:33:31 -0800

    rxbuffer haha

--- robots/TestBench/TestBench.cpp
+++ robots/TestBench/TestBench.cpp
@@ -15,49 +15,11 @@
 #include "main.h"
 #include <string.h>
 
-UART_HandleTypeDef huart2;
-uint8_t rxBuffer[256]; 
+
 uint8_t tempByte;
 
 PID::config test_motor_vel_PID = {1, 0, 0};
 PID::config test_motor_pos_PID = {1, 0, 0};
-
-// declare pin number (bufferedserial)
-BufferedSerial pc(PA_2, PA_3, 115200);  // TX, RX, baud
-
-class TestBench : public BaseRobot {
-  public:
-    DJIMotor motor;
-
-    TestBench(Config &config)
-        : BaseRobot(config),
-          // clang-format off
-        motor(DJIMotor::config{
-            1,
-            CANHandler::CANBUS_1,
-            M3508,
-            "Test motor",
-            test_motor_vel_PID,
-            test_motor_pos_PID
-        })
-        // clang-format on        
-    {}
-
-    ~TestBench() {}
-
-    void init() override 
-    {
-        
-    }
-    
-    void periodic(unsigned long dt_us) override {
-        motor.setPower(0);
-    }
-
-    void end_of_loop() override {}
-
-    unsigned int main_loop_dt_ms() override { return 2; } // 500 Hz loop
-};
 
 struct VTMinput {
 	uint16_t ch0, ch1, ch2, ch3; // 11-bit
@@ -70,10 +32,61 @@ struct VTMinput {
 	uint16_t keyboard;           // 16-bit
 };
 
+class TestBench : public BaseRobot {
+  public:
+    DJIMotor motor;
+	uint8_t rxBuffer[256]; 
+
+	// declare pin number (bufferedserial)
+	BufferedSerial pc;  // TX, RX, baud
+
+    TestBench(Config &config)
+        : BaseRobot(config),
+          // clang-format off
+        motor(DJIMotor::config{
+            1,
+            CANHandler::CANBUS_1,
+            M3508,
+            "Test motor",
+            test_motor_vel_PID,
+            test_motor_pos_PID
+        }),
+		pc(PA_2, PA_3, 921600)
+        // clang-format on        
+    {}
+
+    ~TestBench() {}
+
+    void init() override 
+    {
+        
+    }
+    
+    void periodic(unsigned long dt_us) override {
+		// logic goes here
+        motor.setPower(0);
+		pc.read(rxBuffer, sizeof(rxBuffer));
+		printf("Received data: %s\n", rxBuffer);
+		for(uint8_t i = 0; i < sizeof(rxBuffer); i++) {
+			if(rxBuffer[i] == 0xA9) {
+				printf("Found header at index %d\n", i);
+				// Process the frame starting from this index
+				// For example, if the frame is 10 bytes long:
+				break; // Exit the loop after finding the header
+			}
+		}
+    }
+
+    void end_of_loop() override {}
+
+    unsigned int main_loop_dt_ms() override { return 2; } // 500 Hz loop
+};
+
+
 static framereading(){
 	//find header 1 and header 2
 	while(0xA9) {
-		
+
 	}
 }
 
@@ -81,7 +94,7 @@ int main() {
     printf("HELLO\n");
     BaseRobot::Config config = BaseRobot::Config{}; 
     TestBench TestBench(config);
-	//do everything here (including the code for packets and all that)
+	
 	
 
     TestBench.main_loop();
