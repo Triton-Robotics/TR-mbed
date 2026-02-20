5fabd3e3e971843d489b4d823f2edd43c08c38dc
Edward <chl244@ucsd.edu>
2026-02-20 14:35:52 -0800

    testing ina226

--- robots/TestBench/main.cpp
+++ robots/TestBench/main.cpp
@@ -3,7 +3,7 @@
 #include "mbed.h"
 #include "util/peripherals/INA226_new/ina226.hpp"
 
-I2C i2c(PB_7, PB_6);
+I2C i2c(PB_7, PB_8);
 DigitalOut led(L27);
 DigitalOut led2(L26);
 DigitalOut led3(L25);
@@ -15,7 +15,7 @@ DigitalOut dout(LED1);
 // Serial pc(USBTX, USBRX);
 DigitalOut myled(LED1);
 
-unsigned const int I2C_FREQ = 400000;
+unsigned const int I2C_FREQ = 100000;
 const int ina_addr = 0x80;
 const float current_limit = 1.0;
 ina226 ina(i2c, ina_addr, I2C_FREQ); 
@@ -100,33 +100,55 @@ int main(){
 
     //INA stuff
     printf("INA226 TEST Program. (BUILD:[" __DATE__ "/" __TIME__ "])\n");
-    int count = 1;
 
     // Device configuration. See header file for details.
     printf("INA226 Config return: %d\n", ina.setConfig());  // default configuration
     printf("INA226 Calibration return: %d\n", ina.setCalibration());  // default calibration
     ina.enableShuntOverVoltageAlert();
     ina.setOverCurrentLimit(current_limit);
-    
     while(true){
         timeStart = us_ticker_read();       
 
         if ((timeStart - loopTimer) / 1000 > 1){
-            
-            float elapsedms = (timeStart - loopTimer) / 1000;
             loopTimer = timeStart;
-            led = !led;
-            ledbuiltin = !ledbuiltin;            
+
+            static int debugCounter = 0;
+            if(debugCounter++ % 100 == 0) {  // Only print every 50 loops
+                led = !led;
+                ledbuiltin = !ledbuiltin; 
+                
+                // //INA stuff
+                // printf("Device %xh: ManID %d, DieID %d, Cal %d, ShuntV %+2.6fV, %+2.6fV, %+2.6fA, %+2.6fW\n",
+                // ina_addr,
+                // ina.readManufacturerID(),
+                // ina.readDieID(),
+                // ina.readCalibration(),
+                // ina.readShuntVoltage(),
+                // ina.readBusVoltage(),
+                // ina.readCurrent(),
+                // ina.readPower());
+                // if (ina.isAlert()) {
+                // printf("Overcurrent detected\n");
+                // }  
+
+                printf("\n--- Starting I2C Scanner ---\n");
+                int ack_count = 0;
+                for (int i = 1; i < 128; i++) {
+                    // Mbed I2C write returns 0 on success (ACK received)
+                    if (i2c.write(i << 1, NULL, 0) == 0) { 
+                        printf("SUCCESS: Found I2C device at 7-bit address 0x%02X (8-bit: 0x%02X)\n", i, i << 1);
+                        ack_count++;
+                    }
+                }
+                if (ack_count == 0) {
+                    printf("ERROR: No I2C devices found. Check wiring and power!\n");
+                }
+                printf("--- Scanner Complete ---\n\n");
+            }
+
+        
+                 
             
-            refLoop++;
-            if (refLoop >= 5){                
-                refereeThread(&referee);
-                refLoop = 0;
-                led2 = !led2;            
-            
-            }    
-            
-            // Chassis.periodic();
             remoteRead(); 
 
              //Driving input
@@ -149,156 +171,19 @@ int main(){
             jy = max(-1.0F, min(1.0F, jy));
             jpitch = max(-1.0F, min(1.0F, jpitch));
             jyaw = max(-1.0F, min(1.0F, jyaw));           
-            // Chassis.setChassisSpeeds({jx * Chassis.m_OmniKinematicsLimits.max_Vel,
-            //                               jy * Chassis.m_OmniKinematicsLimits.max_Vel,
-            //                               0 * Chassis.m_OmniKinematicsLimits.max_vOmega}, 100,
-            //                               ChassisSubsystem::YAW_ORIENTED);
-            // motor1.setPower(remote.leftX() * 8000 / 660);
-            // motor2.setPower(remote.leftY() * 8000 / 660);
-            // motor3.setPower(remote.rightX() * 8000 / 660);
-            // motor4.setPower(remote.rightY() * 8000 / 660);
-            // static int statusCounter = 0;
-            // if(statusCounter++ % 50 == 0) {
-            //     if (send_power == true) {
-            //         send_power = false;
-            //     }
-            //     else if (send_power == false) {
-            //         send_power = true;
-            //     }
-            // }
 
             //INDEXER CODE
             if (remote.leftSwitch() == Remote::SwitchState::UP) {
-                // if(robot_status.shooter_barrel_heat_limit < 10 || power_heat_data.shooter_17mm_1_barrel_heat < robot_status.shooter_barrel_heat_limit - 30) {
-                    feeder.setSpeed(5 * 16 * M2006_GEAR_RATIO);
-                    // feeder.setPower(8000);
-                // }
-                // else {
-                //     feeder.setSpeed(0);
-                // }
+                feeder.setSpeed(5 * 16 * M2006_GEAR_RATIO);
             } 
             else {
                 feeder.setPower(0);
             }
-
-            // if (remote.rightSwitch() == Remote::SwitchState::UP) {
-            //     motor1.setSpeed(130*19);
-            //     motor2.setSpeed(130*19);
-            //     motor3.setSpeed(130*19);
-            //     motor4.setSpeed(130*19);
-            //     feeder.setPower(2500);
-            //     RFLYWHEEL.setSpeed(3000);
-            //     LFLYWHEEL.setSpeed(3000);
-            // } else {
-            //     feeder.setPower(0);
-            // }
-
-
-            // if (send_power) {
-            //     motor1.setPower(1000);
-            //     motor2.setPower(1000);
-            //     motor3.setPower(1000);
-            //     motor4.setPower(1000);
-            //     feeder.setSpeed(130*19);
-            //     RFLYWHEEL.setPower(300);
-            //     LFLYWHEEL.setPower(300);
-            // } else {
-            //     motor1.setPower(-1000);
-            //     motor2.setPower(-1000);
-            //     motor3.setPower(-1000);
-            //     motor4.setPower(-1000);
-            //     feeder.setPower(0);
-            //     RFLYWHEEL.setPower(-300);
-            //     LFLYWHEEL.setPower(-300);
-            // }
             
-                      
-  
-            // testMot.setSpeed(remote.leftX() * 3000 / 660);  // Use speed control instead of raw power
-            
-            // Debug PID behavior - Status output every 100ms
-            // static int statusCounte = 0;
-            // if(statusCounte++ % 100 == 0) {
-            //     printff("V=%d P=%d | CHILL?:%s\n",
-            //     feeder.getData(VELOCITY), feeder.getData(POWEROUT), 
-            //     feeder.isConnected() ? "Y" : "N");
-            // }
-
-            // feeder.setSpeed((remote.rightX()/660) * 3000);
-            // RFLYWHEEL.setSpeed((remote.rightY()/660) * 3000);
-            // LFLYWHEEL.setSpeed((remote.rightY()/660) * 3000);
-            
-            // // Debug output - reduce frequency to save memory
-            // static int debugCounter = 0;
-            // if(debugCounter++ % 50 == 0) {  // Only print every 50 loops
-            //     printff("%d %d %d %d\n", 
-            //     remote.rightX(),
-            //     remote.rightY(),
-            //     remote.leftX(),
-            //     remote.leftY());
-            // }
-            
-            // printff("%d,%d\n", testMot>>VELOCITY, testMot>>POWEROUT);            
-            
-        //      static int statusCounter = 0;
-        //      if(statusCounter++ % 50 == 0) {
-
             DJIMotor::s_sendValues();  // Enable debug output
-        //             printff("%d %d %d %d\n",
-        // motor1.getData(VELOCITY)/24,
-        // qmotor2.getData(VELOCITY)/24,
-        // motor3.getData(VELOCITY)/24,
-        // motor4.getData(VELOCITY)/24);
-        //             }
-        
-
-        // Debug output - reduce frequency to save memory
-        // static int debugCounter = 0;
-        // if(debugCounter++ % 50 == 0) {  // Only print every 50 loops
-
-        //     printff(" %d,  %d, %.3f \n", feeder.getData(TORQUE), feeder.getData(POWEROUT), ain.read());
-
-            // float acs_current = readCurrentACS712();
-            // printff("ACS712 Current: %.2f A\n", acs_current);
-            
-            // float acs_torque = calculateTorqueFromACS712(acs_current);
-            // printff("ACS Torque: %7.3f Nm\n", acs_torque);
-
-            // int torque_raw = feeder.getData(TORQUE);
-            // // float torque_calc = TorqueFromCurrent(torque_raw);
-            // // printff("Motor Torque Raw: %4d | Calc: %7.3f Nm\n", torque_raw, torque_calc);
-            // printff("Motor Torque Raw: %4d Nm\n", torque_raw);
-            
-            // float difference = acs_torque - torque_raw;
-            // float percent_diff = (difference / torque_raw) * 100.0f;
-            // printff("Difference: %7.3f Nm (%5.1f%%)\n", difference, percent_diff);
         }
-
-        //INA stuff
-        printf("\n%d:\n", count);
-        printf("Device %xh: ManID %d, DieID %d, Cal %d, ShuntV %+2.6fV, %+2.6fV, %+2.6fA, %+2.6fW\n",
-        ina_addr,
-        ina.readManufacturerID(),
-        ina.readDieID(),
-        ina.readCalibration(),
-        ina.readShuntVoltage(),
-        ina.readBusVoltage(),
-        ina.readCurrent(),
-        ina.readPower());
-        if (ina.isAlert()) {
-        printf("Overcurrent detected\n");
-        }
-
-        // printf("Motor Data: ");
-        // feeder.printAllMotorData();
 
         DJIMotor::s_getFeedback();
         ThisThread::sleep_for(1ms);
-                    // Check if all 4 motors are connected
-            // DJIMotor* motors[4] = {&motor1, &motor2, &motor3, &motor4};
-            // bool allConnected = DJIMotor::s_theseConnected(motors, 4, true);
-            // if (!allConnected) {
-            //     printff("WARNING: Some motors disconnected!\n");
-            // }
     }
 }
