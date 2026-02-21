11e6521fe328e8dce6944e58f4f9cc374d6bc9f5
Rong Shen Ng <ngrongshen2005@gmail.com>
2026-02-21 13:48:50 -0800

    read the next 20 bytes of data

--- robots/TestBench/TestBench.cpp
+++ robots/TestBench/TestBench.cpp
@@ -33,7 +33,8 @@ struct VTMinput {
 class TestBench : public BaseRobot {
   public:
 	uint8_t rxBuffer[50]; 
-
+	// initialize a variable to keep track of whether the header has been found
+	int headerFound = 0;
 	// declare pin number (bufferedserial)
 	BufferedSerial pc;  // TX, RX, baud
 
@@ -60,11 +61,33 @@ class TestBench : public BaseRobot {
         printf("Hello\n");
 		for(unsigned int i = 0; i <= sizeof(rxBuffer); i++) {
 			if((rxBuffer[i] == 0xA9) || (rxBuffer[i] == 0x53)) {
+				headerFound = 1;
 				printf("Found header at index %d\n", i);
-				// Process the frame starting from this index
-				// For example, if the frame is 10 bytes long:
+				// find the next 20 bytes and print them out 
+				// if the header is 0xA9, the next 20 bytes are the data
+				// if the header is 0x53, the next 20 bytes are the data
+				// print out the data in hex format
+				// for example, if the header is 0xA9, print "Data: " followed by the 20 bytes in hex format
+				// if the header is 0x53, print "Data: " followed by the 20 bytes in hex format
+				// if the header is not found, print "Header not found" after the loop
+				if (i + 20 < sizeof(rxBuffer)) {
+                	printf("Data: ");
+                	for (size_t j = 1; j <= 20; j++) {
+                   		printf("%02X ", rxBuffer[i + j]);
+                	}
+                	printf("\n");
+            	} 
+				else {
+                printf("Not enough bytes after header to read 20 bytes\n");
+            	}
+
 				break; // Exit the loop after finding the header
 			}
+			else {
+				headerFound = 0;
+				printf("No header found!\n");
+			}
+				
 		}
     }
 
