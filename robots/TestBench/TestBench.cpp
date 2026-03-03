e5201bc519f151e957dc37f52020b17bc7727c7b
Angie <a1david@ucsd.edu>
2026-03-02 16:09:12 -0800

    added ACS712 functionality into new codebase (waiting for sensor to be delivered)

--- robots/TestBench/TestBench.cpp
+++ robots/TestBench/TestBench.cpp
@@ -12,12 +12,26 @@
 
 #include <algorithm>
 
+#include "mbed.h"
+
 PID::config test_motor_vel_PID = {1, 0, 0};
 PID::config test_motor_pos_PID = {1, 0, 0};
+
+//Constants for ACS712 and M2006 motor
+const float SENSITIVITY = 0.185; //185mV/A for 5A model
+const float V_REF = 3.3;
+const float KT_M2006 = 0.18; // Torque constant for M2006 motor, in Nm/A
 
 class TestBench : public BaseRobot {
   public:
     DJIMotor motor;
+    //Declare AnalogIn for current sensing
+    AnalogIn ain;
+
+//Varibales for current sensing
+float current_amps = 0.0;
+float torque_nm = 0.0; 
+float offset_voltage = 1.65; // Initial guess for offset voltage, will be calibrated in init() 
 
     TestBench(Config &config)
         : BaseRobot(config),
@@ -25,22 +39,53 @@ class TestBench : public BaseRobot {
         motor(DJIMotor::config{
             1,
             CANHandler::CANBUS_1,
-            M3508,
+            M2006, 
             "Test motor",
             test_motor_vel_PID,
             test_motor_pos_PID
-        })
+        }),
+        ain(PA_7)
         // clang-format on        
     {}
+
+    //Torque Calculation From ACS712 Current Sensor
+    float calculateTorque(float amps) {
+        return amps * KT_M2006;
+    }
 
     ~TestBench() {}
 
     void init() override 
     {
-        
+        printf("Calibrating ACS712...don't draw curent.\n");
+        // Calibrate the ACS712 by taking multiple readings and averaging them to find the offset voltage
+        float sum = 0;
+        int samples = 100;
+        for (int i = 0; i < samples; i++) {
+            sum += ain.read() * V_REF;
+            ThisThread::sleep_for(10ms);
+    }
+    
+    float offset_voltage = sum / samples;
+    printf("Offset voltage: %.2f V\n", offset_voltage);
+    
     }
     
     void periodic(unsigned long dt_us) override {
+        
+        //Calculate Voltage from AnalogIn reading
+        float voltage = ain.read() * V_REF;
+
+        //Calculate Current from Voltage and Sensitivity
+        current_amps = (voltage - offset_voltage) / SENSITIVITY; // Subtract offset voltage (1.65V for 0A)
+
+        //Calculate Torque from Current
+        torque_nm = calculateTorque(current_amps);
+
+        // Print current and torque values
+        printf("Current: %.2f A, Torque: %.4f Nm\n", current_amps, torque_nm);
+
+        //
         motor.setPower(0);
     }
 
