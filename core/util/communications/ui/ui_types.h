503d87c9100ca6e20ac79f8de6fd8988d7f88e30
phomebrew <stevenhsiao4@gmail.com>
2026-05-16 14:26:58 -0700

    Condensed Packet Structures - Used token operators to simplify figure types (and also make it like inheritance) - Redid how we prevented frame packing

--- core/util/communications/ui/ui_types.h
+++ core/util/communications/ui/ui_types.h
@@ -24,116 +24,7 @@
 
 // Structures for various figures/numbers/strings
 
-#pragma pack(push, 1)
-typedef struct {
-uint8_t figure_name[3];
-uint32_t operate_type:3;
-uint32_t figure_type:3;
-uint32_t layer:4;
-uint32_t color:4;
-uint32_t _a:9;
-uint32_t _b:9;
-uint32_t width:10;
-uint32_t start_x:11;
-uint32_t start_y:11;
-uint32_t _c:10;
-uint32_t _d:11;
-uint32_t _e:11;
-} ui_interface_figure_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-uint8_t figure_name[3];
-uint32_t operate_type:3;
-uint32_t figure_type:3;
-uint32_t layer:4;
-uint32_t color:4;
-uint32_t _a:9;
-uint32_t _b:9;
-uint32_t width:10;
-uint32_t start_x:11;
-uint32_t start_y:11;
-uint32_t _c:10;
-uint32_t end_x:11;
-uint32_t end_y:11;
-} ui_interface_line_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-uint8_t figure_name[3];
-uint32_t operate_type:3;
-uint32_t figure_type:3;
-uint32_t layer:4;
-uint32_t color:4;
-uint32_t _a:9;
-uint32_t _b:9;
-uint32_t width:10;
-uint32_t start_x:11;
-uint32_t start_y:11;
-uint32_t _c:10;
-uint32_t end_x:11;
-uint32_t end_y:11;
-} ui_interface_rect_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-uint8_t figure_name[3];
-uint32_t operate_type:3;
-uint32_t figure_type:3;
-uint32_t layer:4;
-uint32_t color:4;
-uint32_t _a:9;
-uint32_t _b:9;
-uint32_t width:10;
-uint32_t start_x:11;
-uint32_t start_y:11;
-uint32_t r:10;
-uint32_t _d:11;
-uint32_t _e:11;
-} ui_interface_round_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-uint8_t figure_name[3];
-uint32_t operate_type:3;
-uint32_t figure_type:3;
-uint32_t layer:4;
-uint32_t color:4;
-uint32_t _a:9;
-uint32_t _b:9;
-uint32_t width:10;
-uint32_t start_x:11;
-uint32_t start_y:11;
-uint32_t _c:10;
-uint32_t rx:11;
-uint32_t ry:11;
-} ui_interface_ellipse_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
-uint8_t figure_name[3];
-uint32_t operate_type:3;
-uint32_t figure_type:3;
-uint32_t layer:4;
-uint32_t color:4;
-uint32_t start_angle:9;
-uint32_t end_angle:9;
-uint32_t width:10;
-uint32_t start_x:11;
-uint32_t start_y:11;
-uint32_t _c:10;
-uint32_t rx:11;
-uint32_t ry:11;
-} ui_interface_arc_t;
-#pragma pack(pop)
-
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
     uint8_t figure_name[3];
     uint32_t operate_type: 3;
     uint32_t figure_type: 3;
@@ -146,10 +37,8 @@ typedef struct {
     uint32_t start_y: 11;
     int32_t number;
 } MESSAGE_PACKED ui_interface_number_t;
-#pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
     uint8_t figure_name[3];
     uint32_t operate_type: 3;
     uint32_t figure_type: 3;
@@ -165,70 +54,84 @@ typedef struct {
     uint32_t _e: 11;
     char string[30];
 } MESSAGE_PACKED ui_interface_string_t;
-#pragma pack(pop)
+
+#define PRIMITIVE_CAT(x, y) x ## y
+#define CAT(x, y) PRIMITIVE_CAT(x, y)
+
+// Creates generic figure interface struct (good for inheritance related functions)
+#define DEFINE_MESSAGE(name, p_a, p_b, p_c, p_d, p_e)   \
+typedef struct  __attribute__((__packed__)) {           \
+    uint8_t figure_name[3];                             \
+    uint32_t operate_type:3;                            \
+    uint32_t figure_type:3;                             \
+    uint32_t layer:4;                                   \
+    uint32_t color:4;                                   \
+    uint32_t PRIMITIVE_CAT(,p_a) :9;                    \
+    uint32_t PRIMITIVE_CAT(,p_b):9;                     \
+    uint32_t width:10;                                  \
+    uint32_t start_x:11;                                \
+    uint32_t start_y:11;                                \
+    uint32_t PRIMITIVE_CAT(,p_c):10;                    \
+    uint32_t PRIMITIVE_CAT(,p_d):11;                    \
+    uint32_t PRIMITIVE_CAT(,p_e):11;                    \
+} MESSAGE_PACKED ui_interface_ ## name ##_t             
+
+// Explicitly define figure types with right names
+DEFINE_MESSAGE(figure, _a, _b, _c, _d, _e);
+DEFINE_MESSAGE(line, _a, _b, _c, end_x, end_y);
+DEFINE_MESSAGE(rect, _a, _b, _c, end_x, end_y);
+DEFINE_MESSAGE(round, _a, _b, r, _d, _e);
+DEFINE_MESSAGE(ellipse, _a, _b, _c, rx, ry);
+DEFINE_MESSAGE(arc, start_angle, end_angle, _c, rx, ry);
 
 // Structure for frame header
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
     uint8_t SOF;
     uint16_t length;
     uint8_t seq, crc8;
     uint16_t cmd_id, sub_id;
     uint16_t send_id, recv_id;
 } MESSAGE_PACKED ui_frame_header_t;
-#pragma pack(pop)
 
 // Structure for sending figure data in various sized packets
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
 ui_frame_header_t header;
 ui_interface_figure_t data[1];
 uint16_t crc16;
 } MESSAGE_PACKED ui_1_frame_t;
-#pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
 ui_frame_header_t header;
 ui_interface_figure_t data[2];
 uint16_t crc16;
 } MESSAGE_PACKED ui_2_frame_t;
-#pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
 ui_frame_header_t header;
 ui_interface_figure_t data[5];
 uint16_t crc16;
 } MESSAGE_PACKED ui_5_frame_t;
-#pragma pack(pop)
 
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
 ui_frame_header_t header;
 ui_interface_figure_t data[7];
 uint16_t crc16;
 } MESSAGE_PACKED ui_7_frame_t;
-#pragma pack(pop)
 
 // Structure for sending string data in packets
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
     ui_frame_header_t header;
     ui_interface_string_t option;
     uint16_t crc16;
 } MESSAGE_PACKED ui_string_frame_t;
-#pragma pack(pop)
 
 // Structure for deleting layers
-#pragma pack(push, 1)
-typedef struct {
+typedef struct __attribute__((__packed__)) {
     ui_frame_header_t header;
     uint8_t delete_type;
     uint8_t layer;
     uint16_t crc16;
 } MESSAGE_PACKED ui_delete_frame_t;
-#pragma pack(pop)
 
 // Grabs these variables from another file
 extern ui_string_frame_t _ui_string_frame;
